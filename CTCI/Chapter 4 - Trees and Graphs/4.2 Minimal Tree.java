b62e042c17b97abb762dc43ca79377e897f74709
tech-boy <darshak_mehta@live.com>
2018-07-01 23:05:42 -0400

    Solutions to Chapter 4

--- CTCI/Chapter 4 - Trees and Graphs/4.2 Minimal Tree.java
+++ CTCI/Chapter 4 - Trees and Graphs/4.2 Minimal Tree.java
@@ -137,27 +137,15 @@ class BinarySearchTree {
     	return node;
     }
     
-    public boolean isBSTUtil(Node root, int min, int max) {
-    	if(root == null) //Empty tree is BST
-    		return true;
-    	if(root.key < min || root.key > max)
-    		return false;
-    	return (isBSTUtil(root.left, min, root.key -1) && isBSTUtil(root.right, root.key + 1, max));
-    }
 
-    public boolean isBSTUtilUsingPointers(Node root, Node left, Node right) {
-    	if(root == null) //Empty tree is BST
-    		return true;
-    	if(left != null && root.key < left.key)
-    		return false;
-    	if(right != null && root.key > right.key)
-    		return false;
-    	return (isBSTUtilUsingPointers(root.left, left, root) && isBSTUtilUsingPointers(root.right, root, right));	
+    public int heightUtil(Node root) {
+    	if(root == null)
+    		return 0;
+    	return 1 + Math.max(heightUtil(root.left), heightUtil(root.right));
     }
-
-    public boolean isBST() {
-    	return isBSTUtil(root, Integer.MIN_VALUE,Integer.MAX_VALUE);
-    	//return isBSTUtilUsingPointers(root, null, null);
+ 
+    public int height() {
+    	return heightUtil(root);
     }
     
 	
@@ -166,34 +154,6 @@ class BinarySearchTree {
 		int a[] = {1, 2, 5, 18, 19, 25, 47, 58, 96, 100, 121, 190};
 		bst.root = bst.createMinimalTree(a, 0, a.length - 1);
 		bst.inorder();
-		System.out.println(bst.isBST());
+		System.out.println(bst.height());
 	}	
 }
-
-
-
-/***
-
-TODO: Check if it is a BST
-
-Implementation:
-
-Method 1: Simple and Wrong [O(n)]
-
-Check if left < root and right > root [We miss the depth ndoes with small or large values]; further recursive calls
-
-Method 2: Correct and inefficient [O(n^2)]
-
-Check if maxValue(left) > root || minValue(right) < root; further recursive calls
-
-Method 3: Correct and efficient [O(n)] Space: O(1) if function call stack size is not considered, otherwise O(n)
-
-Method 3 Solution 1: Maintain min, max value at every recursive call 
-Method 3 Solution 2: you can use pointers instead of min max
-
-Method 4: Using in-order traversal [O(n)]
-
-Solution 1: In-order traversal and store in temp array, and check if array is sorted.
-Solution 2: Space optimization: We can keep track of previously visited node. If the value of the currently visited node is less than the previous value, then tree is not BST.
-
-***/
