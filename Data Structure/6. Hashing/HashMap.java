bf85c3660347eb0d62947dd5bd3af5280f18570e
tech-boy <darshak_mehta@live.com>
2018-07-06 17:55:47 -0400

    Hashing template

---
+++ Data Structure/6. Hashing/HashMap.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Solution
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+
+
+		//Iterate HashMap
+		Set set = hmap.entrySet();
+		Iterator iter = set.iterator();
+		while(iter.hasNext()) {
+			Map.Entry pair = (Map.Entry) iter.next();
+			System.out.println(pair.getKey() + " " + pair.getValue());
+		}
+	}
+}
+
