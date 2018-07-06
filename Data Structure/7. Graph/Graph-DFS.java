465a6df5e6028a4c142a4d0757d9ccec998aefe3
tech-boy <darshak_mehta@live.com>
2018-07-06 14:08:46 -0400

    Representation of graph and BFS, DFS implementation

---
+++ Data Structure/7. Graph/Graph-DFS.java
@@ -0,0 +1,75 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Graph
+{
+
+	int V;
+	LinkedList<Integer> adjListArray[];
+
+	Graph(int V) {
+		this.V = V;
+		adjListArray = new LinkedList[V]; //generic array creation
+		for(int i = 0; i < V ; i++){
+            adjListArray[i] = new LinkedList();
+        }
+	}
+
+	public void addEdge(int u, int v) {
+		this.adjListArray[u].addFirst(v);
+		this.adjListArray[v].addFirst(u);
+	}
+
+	public void printGraph() {
+		for(int v = 0; v < V; v++) {
+			System.out.println("Adjacency list of vertex "+ v);
+            System.out.print("head");
+            for(Integer pCrawl: adjListArray[v]){
+                System.out.print(" -> "+pCrawl);
+            }
+            System.out.println("\n");
+		}
+	}
+
+	public void DFSUtil(int s, boolean visited[]) {
+		visited[s] = true;
+		System.out.println(s + " ");
+
+		Iterator<Integer> i = adjListArray[s].listIterator();
+			while(i.hasNext()) {
+				int n = i.next();
+				if( !visited[n]) {
+					DFSUtil(n, visited);
+				}
+			}
+	}
+
+	public void DFS(int s) {
+		//Mark all vertices as not visited
+		boolean visited[] = new boolean[V];
+		DFSUtil(s, visited);
+	}
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int V = 5;
+		Graph graph = new Graph(V);
+		graph.addEdge(0, 1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.printGraph();
+        graph.DFS(0);
+	}
+}
+
+/***
+
+TODO: Implement using STACK data structure, iteratively. 
+
+***/
