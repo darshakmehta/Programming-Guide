e2cdca42b2eefe94a3cb57105a78936c33805d12
tech-boy <darshak_mehta@live.com>
2018-07-10 19:34:41 -0400

    [DP] Number of ways to cover a distance like fibonacci

---
+++ Algorithms/3. Algorithm Paradigm/2. Dynamic Programming/WaysToCoverDistance.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class WaysToCoverDistance
+{
+	public static int findWaysToCoverDistance(int n) {
+		int dp[] = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for(int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		}
+		return dp[n];
+	}
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = 10;
+		System.out.println(findWaysToCoverDistance(n));
+	}
+}
+
+
+/***
+
+Naive Solution is exponential, in worst case O(3^n)
+DP Solution is O(n)
+
+***/
