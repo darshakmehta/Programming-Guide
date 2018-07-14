c76e5240c001e53db2f7de00a0c61d13cc54c7ff
tech-boy <darshak_mehta@live.com>
2018-07-14 18:28:19 -0400

    Selection sort

--- Algorithms/2. Topicwise/2. Sorting/4. Selection Sort/selection-sort.java
+++ Algorithms/2. Topicwise/2. Sorting/4. Selection Sort/selection-sort.java
@@ -1,11 +1,8 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class Solution
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -14,13 +11,13 @@ class Ideone
 		int a[] = new int[size];
 		for(int i = 0; i < size; i++)
 			a[i] = sc.nextInt();
-		Ideone ideone = new Ideone();
-		ideone.selectionSort(a);
+		Solution solution = new Solution();
+		solution.selectionSort(a);
 		for(int i = 0 ;i < a.length; i++)
 		System.out.print(a[i] + " ");
 	}
 	
-	public void selectionSort(int a[]){
+	public void selectionSort(int a[]){ //simple but inefficient 
 		int min, temp;
 		for(int i = 0; i< a.length - 1; i++){
 			min = i;	
@@ -35,3 +32,10 @@ class Ideone
 		}
 	}
 }
+
+/***
+
+Running time is O(n^2)
+O(1) space complexity.
+
+***/
