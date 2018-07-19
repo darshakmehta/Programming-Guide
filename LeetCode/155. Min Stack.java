9effb19f1a7f233485dd8a1bafe605a142a9465f
tech-boy <darshak_mehta@live.com>
2018-07-18 22:18:36 -0400

    implement stack to maintain minimum element

---
+++ LeetCode/155. Min Stack.java
@@ -0,0 +1,70 @@
+class MinStack {
+    
+    Node top;
+    
+    class Node {
+        
+        Node next;
+        int min;
+        int data;
+        
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.min = Integer.MAX_VALUE;
+        }
+    }
+    
+    /** initialize your data structure here. */
+    public MinStack() {
+        top = null;
+    }
+    
+    public void push(int x) {
+        Node new_node = new Node(x);
+        if(top == null) {
+            top = new_node;   
+            //top.min = x;
+        }
+        System.out.println(top.min + " " + x);
+        new_node.min = Math.min(top.min, x);
+        new_node.next = top;
+        top = new_node;
+    }
+    
+    public void pop() {
+        if(top == null)
+            return;
+        top.min = Integer.MAX_VALUE;
+        top = top.next;
+        
+    }
+    
+    public int top() {
+        if(top == null)
+            return -1;
+        return top.data;
+    }
+    
+    public int getMin() {
+        if(top == null)
+            return -1;
+        return top.min;
+    }
+}
+
+/***
+
+TODO: Using 2 stack, as saving one data field on each node, will take large space. We can have second stack where we keep track of min.
+Refer CTCI/Chapter 3 - Stacks and Queues/3.2 Stack Min.java
+
+***/
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
