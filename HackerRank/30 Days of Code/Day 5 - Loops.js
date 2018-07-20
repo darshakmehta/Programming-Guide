b23f92820ded110e29576f6577c7d119bf55b7a4
tech-boy <darshak_mehta@live.com>
2018-07-19 22:53:21 -0400

    Loops in JAVA

---
+++ HackerRank/30 Days of Code/Day 5 - Loops.js
@@ -0,0 +1,32 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const n = parseInt(readLine(), 10);
+    for(let j = 1; j <= 10; j++) {
+        console.log(n + " x " + j + " = " + n * j);
+    }
+}
