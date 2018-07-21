6505b395873adad0a2b634c2d63cbe8e3ffe67c7
tech-boy <darshak_mehta@live.com>
2018-07-21 13:22:31 -0400

    Interfaces in java

---
+++ HackerRank/30 Days of Code/Day 19 - Interfaces.java
@@ -0,0 +1,13 @@
+
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int n) {
+        
+        int sum = n;
+        
+        for(int i = 1; i <= n/2; i++) {
+            if(n % i == 0)
+                sum += i;
+        }
+        return sum;
+    }
+}
