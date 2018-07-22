63d59026e07cc3c88faefc4e420f4d13d775c085
tech-boy <darshak_mehta@live.com>
2018-07-22 11:57:10 -0400

    CodeChef Template

---
+++ codechef-template.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Solution
+{
+
+	public void swap(int a[], int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = a[i];
+	}
+
+	
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-- > 0) {
+			
+			
+			
+			
+		}
+	}
+}
