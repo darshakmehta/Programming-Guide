d06a456240eedcd39056ec628dc23174c8d14585
tech-boy <darshak_mehta@live.com>
2018-07-22 01:50:14 -0400

    Bubble sort

---
+++ HackerRank/30 Days of Code/Day 20 - Sorting.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        // Write Your Code Here
+        boolean isSorted = false;
+        int lastUnsorted = a.length - 1;
+        int countSwaps = 0;
+        while(!isSorted) {
+            isSorted = true;
+            for(int i = 0; i < lastUnsorted; i++) {
+                if(a[i] > a[i+1]) {
+                    int temp = a[i];
+                    a[i] = a[i+1];
+                    a[i+1] = temp;
+                    isSorted = false;
+                    countSwaps++;
+                }
+            }
+            lastUnsorted--;
+        }
+        System.out.println("Array is sorted in " + countSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[a.length - 1]);
+    }
+}
