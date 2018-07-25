a6fc396579d0d2bd7968f5f5005a3dea3e683200
Darshak Mehta <darshak_mehta@live.com>
2018-07-25 10:41:11 -0400

    Cakewalk Math

---
+++ HackerRank/Algorithms/Implementation/Viral Advertising.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the viralAdvertising function below.
+    static int viralAdvertising(int n) {
+        int sum = 0;
+        int count = 5;
+        for(int i = 1; i <= n; i++) {
+            int like = (int) (Math.floor(count / 2));
+            sum += like;
+            count = like * 3;
+        }
+        return sum;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
