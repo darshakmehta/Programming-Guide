a3035196762606e44a2ca2d575fad9ab11a603cf
Darshak Mehta <darshak_mehta@live.com>
2018-07-25 11:47:49 -0400

    Cakewalk Math

---
+++ HackerRank/Algorithms/Implementation/Utopian Tree/Utopian Tree.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the utopianTree function below.
+    static int utopianTree(int n) {
+        int result = 1;
+        if(n == 0)
+            return result;
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 != 0)
+                result = result * 2;
+            else
+                result = result + 1;
+        }
+        return result;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = utopianTree(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
