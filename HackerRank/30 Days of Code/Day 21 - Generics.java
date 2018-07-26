216a65af2967a7c224f85fc78f6bec89d47c351d
Darshak Mehta <darshak_mehta@live.com>
2018-07-26 17:30:38 -0400

    Generics in Java

---
+++ HackerRank/30 Days of Code/Day 21 - Generics.java
@@ -0,0 +1,12 @@
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+    
+    // Write your code here
+    public void printArray(T[] a) {
+        for(T i : a) {
+            System.out.println(i);
+        }
+    }
