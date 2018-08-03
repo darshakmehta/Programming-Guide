5da499cb7c55d55d8d00e7a26cf6ad8d12e7e223
tech-boy <darshak_mehta@live.com>
2018-08-02 23:29:26 -0400

    LinkedList Middle element

---
+++ LeetCode/876. Middle of the Linked List.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        
+        ListNode first = head;
+        ListNode second = head.next;
+        ListNode result = null;
+        while(second != null) {
+            if(second.next != null) {
+                first = first.next;
+                second = second.next.next;
+                result = first;
+            } else {
+                //result = first.next;
+                break;
+            }
+        }
+        return result;
+    }
+}
