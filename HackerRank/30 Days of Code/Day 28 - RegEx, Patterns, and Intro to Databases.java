7f6fc75322fad78c332f45e0ab61fbc16071bada
tech-boy <darshak_mehta@live.com>
2018-08-16 23:43:30 -0400

    String sort

--- HackerRank/30 Days of Code/Day 28 - RegEx, Patterns, and Intro to Databases.java
+++ HackerRank/30 Days of Code/Day 28 - RegEx, Patterns, and Intro to Databases.java
@@ -10,7 +10,6 @@ import java.util.regex.*;
 public class Solution {
  
     private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
