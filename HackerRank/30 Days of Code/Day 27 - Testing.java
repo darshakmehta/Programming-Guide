20df45e41ec6ae6e7e46eb3263dfda58ff16533b
tech-boy <darshak_mehta@live.com>
2018-08-16 23:43:10 -0400

    Testing and String sort

---
+++ HackerRank/30 Days of Code/Day 27 - Testing.java
@@ -0,0 +1,34 @@
+static class TestDataEmptyArray {
+    public static int[] get_array() {
+        // complete this function
+        int arr[] = {};
+        return arr;
+    }
+}
+
+static class TestDataUniqueValues {
+    public static int[] get_array() {
+        // complete this function
+        int a[] = {2, 1, 3, 4, 5};
+        return a;
+    }
+
+    public static int get_expected_result() {
+        // complete this function
+        return 1;  
+    }
+}
+
+static class TestDataExactlyTwoDifferentMinimums {
+   
+    public static int[] get_array() {
+        // complete this function
+       int a[] = {1, 2, 1};
+        return a;
+    }
+
+    public static int get_expected_result() {
+        // complete this function
+        return 0;   
+    }
+}
---
+++ HackerRank/30 Days of Code/Day 28 - RegEx, Patterns, and Intro to Databases.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Solution {
+ 
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        ArrayList<String> persons = new ArrayList<String>();
+        for (int NItr = 0; NItr < N; NItr++) {
+            String[] firstNameEmailID = scanner.nextLine().split(" ");
+
+            String firstName = firstNameEmailID[0];
+
+            String emailID = firstNameEmailID[1];
+            if(emailID.contains("@gmail.com")) {
+                persons.add(firstName);
+            }
+        }
+        
+        Collections.sort(persons);
+        for(String s : persons) {
+            System.out.println(s);
+        }
+
+        scanner.close();
+    }
+}
