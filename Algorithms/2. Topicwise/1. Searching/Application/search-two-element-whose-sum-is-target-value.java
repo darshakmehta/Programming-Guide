0bd54985863a00077a69e9ada193b491f460dd64
tech-boy <darshak_mehta@live.com>
2018-10-04 22:17:44 -0400

    Solution with duplicate values

--- Algorithms/2. Topicwise/1. Searching/Application/search-two-element-whose-sum-is-target-value.java
+++ Algorithms/2. Topicwise/1. Searching/Application/search-two-element-whose-sum-is-target-value.java
@@ -70,5 +70,26 @@ PAIR-EXISTS(S, x):
 
   return false
 
+// Solution 3
+
+Using HashSet we cannot achieve as it cant have duplicate values of array
+Using HashMap where key is number in the array (duplicate keys not allowed) and value is the index so we cannot achieve
+
+
+O(n^2) solution
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            for(int j = 0; j < nums.length && j != i; j++) {
+                if(x == nums[j]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{0, 0};
+    }
+}
 
 **/
