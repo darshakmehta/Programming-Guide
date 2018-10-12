f1f1327f0c7a0f2c43c3f6fbc6c0b79a09677a23
tech-boy <darshak_mehta@live.com>
2018-10-12 02:39:59 -0400

    Removed unnecessary code

--- LeetCode/387. First Unique Character in a String.java
+++ LeetCode/387. First Unique Character in a String.java
@@ -1,19 +1,15 @@
 class Solution {
     public int firstUniqChar(String s) {
-        LinkedHashMap<Character, Integer> hmap = new LinkedHashMap<Character, Integer>();
+        Map<Character, Integer> hmap = new HashMap<Character, Integer>();
         if(s.length() == 0)
             return -1;
         char c[] = s.toCharArray();
-        int a[] = new int[s.length()];
         for(int i = 0; i < c.length; i++) {
             int count = 1;
             if(hmap.containsKey(c[i])) {
-                count = hmap.get(c[i]);
-                count++;
+                count = hmap.get(c[i]) + 1;
             }
             hmap.put(c[i],count);
-            a[i] = count;
-            //System.out.println(c[i] + " " + a[i] + " " + i);
         }
         for( int i = 0; i < c.length; i++) {
             if(hmap.get(c[i]) == 1) {
@@ -21,7 +17,5 @@ class Solution {
             }
         }
         return -1;
-        
-        
     }
 }
