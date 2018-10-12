8d4b00f337dcecceb9fc51f9b44e4a18014c30c7
tech-boy <darshak_mehta@live.com>
2018-10-12 11:32:43 -0400

    Substring technique

--- LeetCode/28. Implement strStr().java
+++ LeetCode/28. Implement strStr().java
@@ -30,3 +30,18 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        if(haystack.length() < needle.length()) return -1;
+        else if(haystack.length() == 0 && needle.length() == 0) return 0;
+        
+        for(int i = 0; i <= (haystack.length() - needle.length()); i++) {
+            if(haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
