09933a73d20cba3b53ac52b4b12c3372f262b1f3
tech-boy <darshak_mehta@live.com>
2018-10-12 02:14:45 -0400

    Reverse String using Character Array and Swap - 2 pointer

--- LeetCode/344. Reverse String.java
+++ LeetCode/344. Reverse String.java
@@ -11,11 +11,39 @@ class Solution {
 }
 
 
+class Solution {
+    public String reverseString(String s) {
+        StringBuffer sb = new StringBuffer("");
+        for(int i = s.length() - 1; i >= 0; i--) {
+            sb.append(s.charAt(i));
+        }
+        
+        return sb.toString();
+    }
+}
+
+class Solution { //Efficient than above 2 solutions
+    public String reverseString(String s) {
+        char ch[] = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while(i < j) {
+            char temp = ch[i];
+            ch[i] = ch[j];
+            ch[j] = temp;
+            i++; j--;
+        }
+        
+        return new String(ch);
+    }
+}
+
 /*** Method 1: Converting String into Bytes ***/
-/*** Method 2: Using built in reverse() method of the StringBuilder class ***/
-/*** Method 3: Converting String to character array ***/
+/*** Method 2: Using built in reverse() method of the StringBuilder class {Solution 1} ***/ 
+/*** Method 3: Converting String to character array {Solution 3} ***/ 
 /*** Method 4: Convert the input string into character array by using the toCharArray() and Reverse a String using swapping ***/
-/*** Method 5: Using ArrayList object ***/
+/*** Method 5: Using ArrayList object or StringBuffer.append() {Solution 2} ***/
 
 //Reference: geeksforgeeks
 
