700da99b51dd48df69e5d4223c93f13d6cbfef32
tech-boy <darshak_mehta@live.com>
2018-10-12 23:54:52 -0400

    Stack - Insert index value instead of parantheses

---
+++ LeetCode/32. Longest Valid Parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(-1);
+        int maxLength = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if(!stack.isEmpty()) {
+                    int x = i - stack.peek();
+                    maxLength = maxLength > x ? maxLength : x;
+                } else {
+                    stack.push(i);
+                }
+            }
+        }
+        return maxLength;
+        
+    }
+}
