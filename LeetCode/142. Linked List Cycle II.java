cc0c5bdab6acaac7eee1c14d8fe1b6bcb0cd00f1
tech-boy <darshak_mehta@live.com>
2018-10-13 02:33:05 -0400

    Return intersection node

---
+++ LeetCode/142. Linked List Cycle II.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+		ListNode fast = head;
+
+		//find collision
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if(slow == fast) //collide
+				break;
+		}
+
+		//No meeting point, no loop
+		if(fast == null || fast.next == null) {
+			return null;
+		}
+
+		//find loop start
+		slow = head;
+		while(slow != fast) {
+			slow = slow.next;
+			fast = fast.next;
+		}
+
+		return slow;
+    }
+}
