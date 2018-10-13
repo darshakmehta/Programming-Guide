704dc98ae415bcfff2ce0ec4d67374bb4c6e7fd0
tech-boy <darshak_mehta@live.com>
2018-10-13 11:48:23 -0400

    Improved code

--- LeetCode/204. Count Primes.java
+++ LeetCode/204. Count Primes.java
@@ -1,20 +1,18 @@
 class Solution {
     public int countPrimes(int n) {
-        boolean a[] = new boolean[n];
-        for(int i = 2; i < n; i++)
-            a[i] = true;
-        
+        boolean flag[] = new boolean[n];
         for(int i = 2; i <= Math.sqrt(n); i++) {
-            if(a[i]) {
-                for(int j = i * i; j < n ; j+=i) {
-                    a[j] = false;
+            if(!flag[i]) {
+                for(int j = i * i; j < n; j += i) {
+                    flag[j] = true;
                 }
             }
         }
         int count = 0;
-        for(int i = 2; i < n; i++)
-            if(a[i])
+        for(int i = 2; i < n; i++) {
+            if(!flag[i])
                 count++;
+        }
         return count;
     }
 }
