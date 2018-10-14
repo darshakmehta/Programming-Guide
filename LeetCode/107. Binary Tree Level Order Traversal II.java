34f1f14526e8acd9f0b3d64fcdbe10b69ac60f53
tech-boy <darshak_mehta@live.com>
2018-10-14 09:24:58 -0400

    Reverse Tree level order traversal

---
+++ LeetCode/107. Binary Tree Level Order Traversal II.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void levelHelper(List<List<Integer>> result, TreeNode root, int height) {
+        if (root == null) return;
+        if (height >= result.size()) {
+            result.add(new LinkedList<Integer>());
+        }
+        result.get(height).add(root.val);
+        levelHelper(result, root.left, height+1);
+        levelHelper(result, root.right, height+1);
+    }
+    
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        levelHelper(result, root, 0);
+        Collections.reverse(result);
+        return result;
+    }
+}
