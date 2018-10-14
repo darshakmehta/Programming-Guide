b4ca63f9653b688ea15d1b2df2ea5448af87e4e9
tech-boy <darshak_mehta@live.com>
2018-10-14 19:34:12 -0400

    updated code

--- LeetCode/921. Minimum Add to Make Parentheses Valid.java
+++ LeetCode/921. Minimum Add to Make Parentheses Valid.java
@@ -35,3 +35,30 @@ class Solution {
         return count;
     }
 }
+
+//Required Solution
+class Solution {
+    public int minAddToMakeValid(String S) {
+        Stack<Character> stack = new Stack<Character>();
+        int count = 0;
+        char ch[] = S.toCharArray();
+        for(char c : ch) {
+            if(c == '(') {
+                stack.push(c);
+            } else {
+                if(c == ')' && !stack.isEmpty()) {
+                    if(stack.peek() != '(') {
+                        count++;
+                    } else {
+                        stack.pop();
+                    }
+                } else {
+                    count++;
+                }
+            }
+        }
+        if(!stack.isEmpty())
+            return count + stack.size();
+        return count;
+    }
+}
