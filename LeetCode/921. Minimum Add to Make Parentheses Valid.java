eec7d490fe3a8c2314bfce9617e762e3f21f58c0
tech-boy <darshak_mehta@live.com>
2018-10-14 09:25:55 -0400

    Find number of paratheses required to validate

---
+++ LeetCode/921. Minimum Add to Make Parentheses Valid.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minAddToMakeValid(String S) {
+        Stack<Character> stack = new Stack<Character>();
+        int count = 0;
+        char ch[] = S.toCharArray();
+        for(char c : ch) {
+            if(c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else {
+                if(c == ')' && !stack.isEmpty()) {
+                    if(stack.peek() != '(') {
+                        count++;
+                    } else {
+                        stack.pop();
+                    }
+                } else if(c == '}' && !stack.isEmpty()) {
+                    if(stack.peek() != '{') {
+                        count++;
+                    } else {
+                        stack.pop();
+                    }
+                } else if(c == ']' && !stack.isEmpty()) {
+                    if(stack.peek() != '[') {
+                        count++;
+                    } else {
+                        stack.pop();
+                    }
+                } else {
+                    count++;
+                }
+            }
+        }
+        if(!stack.isEmpty())
+            return count + stack.size();
+        return count;
+    }
+}
