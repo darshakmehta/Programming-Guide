fa569e77b4664e207a18c08f638f2293f3bb9792
tech-boy <darshak_mehta@live.com>
2018-10-13 22:58:13 -0400

    Iterative and recursive solution

--- LeetCode/98. Validate Binary Search Tree.java
+++ LeetCode/98. Validate Binary Search Tree.java
@@ -7,7 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
+class Solution { //Recursive Solution
     
     public boolean isBSTUtilUsingPointers(TreeNode root, TreeNode left, TreeNode right) {
     	if(root == null) //Empty tree is BST
@@ -21,5 +21,35 @@ class Solution {
 
     public boolean isValidBST(TreeNode root) {
         return isBSTUtilUsingPointers(root, null, null);
+    }
+}
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution { //Iterative solution
+
+    public boolean isValidBST(TreeNode root) {
+       if (root == null) return true;
+       Stack<TreeNode> stack = new Stack<>();
+       TreeNode pre = null;
+       while (root != null || !stack.isEmpty()) {
+          while (root != null) {
+             stack.push(root);
+             root = root.left;
+          }
+          root = stack.pop();
+          if(pre != null && root.val <= pre.val) return false;
+          pre = root;
+          root = root.right;
+       }
+       return true;
     }
 }
