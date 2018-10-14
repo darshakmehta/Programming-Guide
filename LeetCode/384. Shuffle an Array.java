6af9158247def220d90be48f2a7f4be7cea44791
tech-boy <darshak_mehta@live.com>
2018-10-13 20:44:55 -0400

    Shuffling an array - deck of cards

---
+++ LeetCode/384. Shuffle an Array.java
@@ -0,0 +1,41 @@
+class Solution { //Fisher-Yates Algorithm
+    private int[] array;
+    private int[] original;
+
+    Random rand = new Random();
+
+    private int randRange(int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private void swap(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public Solution(int[] nums) {
+        array = nums;
+        original = nums.clone();
+    }
+    
+    public int[] reset() {
+        array = original;
+        original = original.clone();
+        return original;
+    }
+    
+    public int[] shuffle() {
+        for (int i = 0; i < array.length; i++) {
+            swap(i, randRange(i, array.length));
+        }
+        return array;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
