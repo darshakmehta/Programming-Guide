9cb65ac0277aab82322fa86db46f24502dc97e93
tech-boy <darshak_mehta@live.com>
2018-10-14 23:54:06 -0400

    Trailing zeroes in factorial using iteration

--- LeetCode/172. Factorial Trailing Zeroes.java
+++ LeetCode/172. Factorial Trailing Zeroes.java
@@ -4,3 +4,14 @@ class Solution {
         return n / 5 + trailingZeroes(n / 5);
     }
 }
+
+class Solution {
+    public int trailingZeroes(int n) {
+        int sum = 0;
+        while(n != 0) {
+            sum += n/5;
+            n = n/5;
+        }
+        return sum;
+    }
+}
