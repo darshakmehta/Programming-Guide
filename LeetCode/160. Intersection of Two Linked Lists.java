83753df4252e8ab4406b7094dda1cfca5796ee71
Mehta <dmehta9@uncc.edu>
2018-10-16 09:30:15 -0400

    Logic behind math property

--- LeetCode/160. Intersection of Two Linked Lists.java
+++ LeetCode/160. Intersection of Two Linked Lists.java
@@ -22,3 +22,21 @@ public class Solution {
         return a;
     }
 }
+
+/***
+
+Consider 
+List 1 = 5 elements; Intersection node is at 3rd element
+List 2 = 6 elements; Intersection node is at 4th element
+
+Now, loop first list and than start looping second list;
+Now, loop second list and than start looping first list;
+
+Therefore List 1 = 5 + 6 (list 2) = 11 elements [first touch at intersection on 3rd and 9th]
+Therefore List 2 = 6 + 5 (list 1) = 11 elements [first touch at intersection on 4th and 9th]
+
+So, if there is intersection point between 2 list, it will be met on 9th position.
+
+Beautiful Math property
+
+***/
