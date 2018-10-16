cee329a7e70abba6e87831749c36e351aca0830a
Mehta <dmehta9@uncc.edu>
2018-10-16 10:20:13 -0400

    Bit Manipulation

--- LeetCode/371. Sum of Two Integers.java
+++ LeetCode/371. Sum of Two Integers.java
@@ -22,3 +22,15 @@ class Solution {
         return count;
     }
 }
+
+// Bit Manipulation
+class Solution {
+    public int getSum(int a, int b) {
+        while(b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+}
