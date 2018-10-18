67c058aef30d42e1da76a430fc2fcc29a3220314
tech-boy <darshak_mehta@live.com>
2018-10-17 21:28:00 -0400

    String 2 contains permutation of string 2

---
+++ LeetCode/567. Permutation in String.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length()>s2.length()) return false;
+
+        Map<Character,Integer> hmap = new HashMap<>();
+        char[] ch=s1.toCharArray();
+        
+        for(char c : ch)
+            hmap.put(c, hmap.getOrDefault(c, 0) + 1);
+        
+        int count=hmap.size();
+        int start=0, end=0;
+        
+        while (end < s2.length()) {
+            char tempc = s2.charAt(end);
+            if (hmap.containsKey(tempc)){
+                hmap.put(tempc, hmap.get(tempc) - 1);
+                if (hmap.get(tempc)==0) count--;
+            }
+
+            end++;
+
+            while (count == 0){
+                tempc=s2.charAt(start);
+                if (hmap.containsKey(tempc)){
+                    hmap.put(tempc, hmap.get(tempc) + 1);
+                    if (hmap.get(tempc)>0) count++;   
+                }
+                if(end - start == s1.length()) return true;
+                start++;
+            } 
+        }
+        return false;
+    }
+}
