06d69565ec1047a68ee29e79e9f2002a35ce63b5
tech-boy <darshak_mehta@live.com>
2018-10-19 09:05:23 -0400

    Find the path whose addition equals to target sum

---
+++ LeetCode/112. Path Sum.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+    
+        if(root.left == null && root.right == null && sum - root.val == 0) return true; // [1] 1 test case
+    
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
