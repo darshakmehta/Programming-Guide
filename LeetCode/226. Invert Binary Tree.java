4c97c928ca61bdd82016438644385383ed34773f
tech-boy <darshak_mehta@live.com>
2018-10-18 23:08:47 -0400

    Invert BT by swapping the left and right node

---
+++ LeetCode/226. Invert Binary Tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution { //Recursive
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
+
+class Solution { //Iterative
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            TreeNode temp = current.left; //swap
+            current.left = current.right;
+            current.right = temp;
+            if (current.left != null) queue.add(current.left);
+            if (current.right != null) queue.add(current.right);
+        }
+        return root;
+    }
+}
