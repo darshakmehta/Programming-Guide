5631cddb0fbdf83f815e9b4b6cd9b1f7ab361486
tech-boy <darshak_mehta@live.com>
2018-10-19 17:15:28 -0400

    Fibonnaci Numbers

--- HackerRank/Interview Preparation Kit/Recursion and Backtracking/Recursion - Fibonacci Numbers.java
+++ HackerRank/Interview Preparation Kit/Recursion and Backtracking/Recursion - Fibonacci Numbers.java
@@ -3,16 +3,10 @@ import java.util.*;
 public class Solution {
 
     public static int fibonacci(int n) {
-        
-        if(n <= 0)
-            return 0;
-        if(n == 1)
-            return 1;
+        if(n <= 0) return 0;
+        if(n == 1) return 1;
         return fibonacci(n - 1) + fibonacci(n - 2);
-
-        // Complete the function.
     }
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
