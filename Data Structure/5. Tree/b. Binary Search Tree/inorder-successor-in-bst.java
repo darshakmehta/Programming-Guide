85a7348364a8753919ba6a51b96e64acd50a3e0f
tech-boy <darshak_mehta@live.com>
2018-10-25 20:32:39 -0400

    TODO: Test all the solutions

--- Data Structure/5. Tree/b. Binary Search Tree/inorder-successor-in-bst.java
+++ Data Structure/5. Tree/b. Binary Search Tree/inorder-successor-in-bst.java
@@ -3,7 +3,7 @@ class Solution {
 		if(root == null || p == null) return;
 
 		if(p.right != null) {
-			return minValue(n.right);
+			return minValue(n.right); //TODO
 		}
 
 		TreeNode successor = null;
@@ -19,5 +19,70 @@ class Solution {
 		}
 		return successor;
 	}
+ 
+}
 
+public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+    if(root==null)
+        return null;
+ 
+    TreeNode next = null;
+    TreeNode c = root;
+    while(c!=null && c.val!=p.val){ //check for p.null
+        if(c.val > p.val){
+            next = c;
+            c = c.left;
+        }else{
+            c= c.right;
+        }
+    }
+ 
+    if(c==null)        
+        return null;
+ 
+    if(c.right==null)
+        return next;
+ 
+    c = c.right;
+    while(c.left!=null)
+        c = c.left;
+ 
+    return c;
+}
+
+public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    if(root==null || p==null)
+        return null;
+ 
+    stack.push(root);
+    boolean isNext = false;
+    while(!stack.isEmpty()){
+        TreeNode top = stack.pop();
+ 
+        if(top.right==null&&top.left==null){
+            if(isNext){
+                return top;
+            }
+ 
+            if(p.val==top.val){
+                isNext = true;
+            }
+            continue;
+        }
+ 
+        if(top.right!=null){
+            stack.push(top.right);
+            top.right=null;
+        }
+ 
+        stack.push(top);
+ 
+        if(top.left!=null){
+            stack.push(top.left);
+            top.left=null;
+        }
+    }
+ 
+    return null;
 }
