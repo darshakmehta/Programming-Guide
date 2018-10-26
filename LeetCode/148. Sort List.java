ca01f339ceebe85bf36f45ea7102d67dcfdcdab6
tech-boy <darshak_mehta@live.com>
2018-10-26 19:41:21 -0400

    Sort LinkedList with O(1) space

---
+++ LeetCode/148. Sort List.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        ListNode first = head, second = head;
+        ListNode minHead = head;
+        while(first != null) {
+            boolean flag = false;
+            if(first.next != null) {
+                second = first.next;
+                while(second != null) {
+                    if(second.val < minHead.val) {
+                        minHead = second;
+                        flag = true;
+                    }
+                    second = second.next;
+                }
+                    if(flag) {
+                        int t = first.val;
+                        first.val = minHead.val;
+                        minHead.val = t;
+                    }
+            }
+            first = first.next;
+            minHead = first;
+        }
+    
+       return head;
+    }
+}
