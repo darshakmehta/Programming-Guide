2ed333187210bb8f7f555e6a69f7ab88bb31d64c
tech-boy <darshak_mehta@live.com>
2018-10-25 20:58:22 -0400

    Maximum area of rectangle

---
+++ LeetCode/11. Container With Most Water.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int water = 0;
+        int left = 0; 
+        int right = height.length - 1;
+        
+        while(left < right) {
+            water = Math.max(water, Math.min(height[left], height[right]) * (right - left));
+            if(height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return water;
+    }
+}
