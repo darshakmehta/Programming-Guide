7eeb6d09ee863ec249ab8bb71389cb6a7e1cc2f2
tech-boy <darshak_mehta@live.com>
2018-10-26 23:27:33 -0400

    Greedy Solution

---
+++ LeetCode/134. Gas Station.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sumGas = 0, sumCost = 0, start = 0, tank = 0;
+        for (int i = 0; i < gas.length; i++) {
+            sumGas += gas[i];
+            sumCost += cost[i];
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        if (sumGas < sumCost) {
+            return -1;
+        } else {
+            return start;
+        }
+    }
+}
