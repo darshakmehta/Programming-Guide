69bf6f9c5183d4bf135ef8558ed99c5b79ef2720
tech-boy <darshak_mehta@live.com>
2018-10-26 20:36:14 -0400

    LCA between two nodes in Binary Tree

---
+++ LeetCode/236. Lowest Common Ancestor of a Binary Tree.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return root;
+        if(p == root || q == root) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null)
+            return root;
+        return left != null ? left : right;
+    }
+}
