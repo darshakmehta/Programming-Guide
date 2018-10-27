ccf3ebfba5b00345dc7e19d5633b60846d6530f0
tech-boy <darshak_mehta@live.com>
2018-10-27 12:22:08 -0400

    Copy List with Random Pointers

--- LeetCode/138. Copy List with Random Pointer.java
+++ LeetCode/138. Copy List with Random Pointer.java
@@ -1,3 +1,11 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
 /* Recursive Solution */
 
 public class Solution {
