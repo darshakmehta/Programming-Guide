81b8a9bcaaddd6118d03d388550f623d3b910d16
tech-boy <darshak_mehta@live.com>
2018-10-26 20:41:26 -0400

    Inorder successor in BST

--- LeetCode/285. Inorder Successor in BST.java
+++ LeetCode/285. Inorder Successor in BST.java
@@ -52,35 +52,34 @@ public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
 
 public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
     Stack<TreeNode> stack = new Stack<TreeNode>();
-    if(root==null || p==null)
-        return null;
+    if(root == null || p == null) return null;
  
     stack.push(root);
     boolean isNext = false;
     while(!stack.isEmpty()){
         TreeNode top = stack.pop();
  
-        if(top.right==null&&top.left==null){
+        if(top.right == null && top.left==null){
             if(isNext){
                 return top;
             }
  
-            if(p.val==top.val){
+            if(p.val == top.val){
                 isNext = true;
             }
             continue;
         }
  
-        if(top.right!=null){
+        if(top.right != null){
             stack.push(top.right);
-            top.right=null;
+            top.right = null;
         }
  
         stack.push(top);
  
-        if(top.left!=null){
+        if(top.left != null){
             stack.push(top.left);
-            top.left=null;
+            top.left = null;
         }
     }
  
