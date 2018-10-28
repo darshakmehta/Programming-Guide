8215398f00b458394cc70e63cd2bc04c32b60bb6
tech-boy <darshak_mehta@live.com>
2018-10-27 23:42:49 -0400

    Not a good question

---
+++ LeetCode/932. Beautiful Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] beautifulArray(int N) {
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(1);
+        while (res.size() < N) {
+            ArrayList<Integer> tmp = new ArrayList<>();
+            for (int i : res) if (i * 2 - 1 <= N) tmp.add(i * 2 - 1);
+            for (int i : res) if (i * 2 <= N) tmp.add(i * 2);
+            res = tmp;
+        }
+        return res.stream().mapToInt(i -> i).toArray();
+    }
+}
