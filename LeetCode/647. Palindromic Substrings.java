86c3b88d719053ec55f90609cc2a9b69a52dfecf
Mehta <dmehta9@uncc.edu>
2018-10-29 10:13:49 -0400

    Print all palindromic substrings

--- LeetCode/647. Palindromic Substrings.java
+++ LeetCode/647. Palindromic Substrings.java
@@ -20,6 +20,35 @@ class Solution {
     }
 }
 
+/* Print All Palindromic substrings */
+class Solution {
+    int count = 0;
+    List<String> list = new ArrayList<String>(); //List containing palindromic substrings
+    
+    public void extendPalindrome(String s, int left, int right) {
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) { //Expand Around center
+            list.add(s.substring(left, right + 1));
+            count++;
+            left--;
+            right++;
+        }
+    }
+    
+    public int countSubstrings(String s) {
+        if(s == null || s.length() == 0) return 0;    
+        
+        for(int i = 0; i < s.length(); i++) {
+            extendPalindrome(s, i, i);
+            extendPalindrome(s, i, i + 1);
+        }
+        for(String str : list)
+            System.out.println(str); //Print list of palindromic substrings
+        return count;
+    }
+    /* To print unique substring, add them to HashSet */
+}
+
+
 class Solution {
     int count = 0;
     Map<String, Integer> hmap = new HashMap<String, Integer>(); //to find all the occurences or distinct occurences of substring
@@ -53,9 +82,7 @@ class Solution {
             System.out.println(pair.getKey() + " " + pair.getValue());  
         }
         return count;
-    }
-    
-    
+    }  
 }
 
 
