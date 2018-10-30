bf5460dc2838463d52ee3b3776e66b986bc5cb77
tech-boy <darshak_mehta@live.com>
2018-10-29 23:19:38 -0400

    Move back to i = 0 while deleting

---
+++ HackerRank/Algorithms/Strings/Super Reduced String.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the superReducedString function below.
+    static String superReducedString(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        for(int i = 1; i < sb.length(); i++) {
+            if(sb.charAt(i) == sb.charAt(i - 1)) {
+                sb.delete(i - 1, i + 1);
+                i = 0;
+            }
+        }
+        if(sb.length() == 0)
+            return "Empty String";
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = superReducedString(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
