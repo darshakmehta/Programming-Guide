e7db3fffe5304b9cd3441f3187dd86752cbbd444
tech-boy <darshak_mehta@live.com>
2018-11-02 14:03:57 -0400

    Comments added

--- LeetCode/252. Meeting Rooms.java
+++ LeetCode/252. Meeting Rooms.java
@@ -1,12 +1,13 @@
+/* Determine if a person can attend all the meetings given [start_time, end_time] */
 public boolean canAttendMeetings(Interval[] intervals) {
-    Arrays.sort(intervals, new Comparator<Interval>(){
+    Arrays.sort(intervals, new Comparator<Interval>(){ //Sort an intervals array of [start_time, end_time]
         public int compare(Interval a, Interval b){
             return a.start-b.start;
         }
     });
  
     for(int i=0; i<intervals.length-1; i++){
-        if(intervals[i].end>intervals[i+1].start){
+        if(intervals[i].end>intervals[i+1].start){ //If end time of next meeting is greater than start time of the previous meeting he cannot attend all the meetings
             return false;
         }
     }
