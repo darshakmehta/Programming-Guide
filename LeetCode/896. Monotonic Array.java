0c790515b842a5e24c11fcdc30f6e935b45ad8fd
tech-boy <darshak_mehta@live.com>
2018-11-02 23:48:27 -0400

    Improved style of code

--- LeetCode/896. Monotonic Array.java
+++ LeetCode/896. Monotonic Array.java
@@ -1,25 +1,22 @@
 class Solution {
     public boolean isMonotonic(int[] A) {
-        boolean result = true;
+        boolean flag = true;
+        
         for(int i = 0; i < A.length - 1; i++) {
-            if(!(A[i] <= A[i+1])) {
-                result = false;
+            if(!(A[i] <= A[i+1])) { //check ascending order
+                flag = false;
                 break;
             }
         }
         
-        if(!result) {
-            //System.out.println("Here");
+        if(!flag) {
             for(int i = 0; i < A.length - 1; i++) {
-                if(!(A[i] >= A[i+1])) {
-                    result = false;
-                    break;
-                } else {
-                    result = true;
+                if(!(A[i] >= A[i+1])) { //check descending order
+                    return false;
                 }
             }
         }
         
-        return result;
+        return true;
     }
 }
