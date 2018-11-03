9ce68ee2e7797453d4d987baea09447dbf55cd4d
tech-boy <darshak_mehta@live.com>
2018-11-02 22:37:12 -0400

    Improved by n/2 calls to Math.min

--- LeetCode/561. Array Partition I.java
+++ LeetCode/561. Array Partition I.java
@@ -11,3 +11,19 @@ class Solution {
         return sum;
     }
 }
+
+/* Logic - Removed n/2 calls to Math.min, since our each pair will be selected from even index */
+class Solution { //Improved by 45% to 93%
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums); //Can go to 100% by better sort algorithm
+        
+        int x = nums.length/2;
+        int sum = 0;
+        for(int i = 0; i < nums.length - 1; i+=2) {
+            sum += nums[i]; //Removed n/2 calls to Math.min
+        }
+        
+        return sum;
+    }
+}
+
