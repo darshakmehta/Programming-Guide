596e8b759dfdb35ebe91e34add599a25932275c9
tech-boy <darshak_mehta@live.com>
2018-11-02 23:54:30 -0400

    Optimized the code

--- LeetCode/485. Max Consecutive Ones.java
+++ LeetCode/485. Max Consecutive Ones.java
@@ -13,3 +13,20 @@ class Solution {
         return max;
     }
 }
+
+/* Since the test case will have maximum 1's so, I am finding max when I find a zero to optimize code */
+class Solution { //Improves 26% to 88%
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int consecutiveOne = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 1) {
+                consecutiveOne++;
+            } else {
+                max = Math.max(max, consecutiveOne);
+                consecutiveOne = 0;
+            }
+        }
+        return Math.max(max, consecutiveOne); //For corner case only one element [1]
+    }
+}
