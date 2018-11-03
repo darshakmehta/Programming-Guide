338c9d58c2852747127f493f6cba2b1dc5e0acf5
tech-boy <darshak_mehta@live.com>
2018-11-02 23:30:59 -0400

    Commented incorrect solution

--- LeetCode/243. Shortest Word Distance.java
+++ LeetCode/243. Shortest Word Distance.java
@@ -26,21 +26,21 @@ public class Solution {
     }
 }
 
-/* easy to understand but little inefficent since we check minimum condition for all words in the list, we can improve by writing the minimum condition block inside the if and else if to drastically improve the efficiency */
-class Solution {
-    public int shortestDistance(String[] words, String word1, String word2) {
-        int i = -1, j = -1;
-        int min = Integer.MAX_VALUE;
-        for(int k = 0; k < words.length; i++) {
-            if(words[k].equals(word1)) i = k;
-            else if(words[k].equals(word1)) j = k;
-            if(i != -1 && j != -1)
-                min = Math.min(min, Math.abs(i - j));
-        }
-    }
-}
+/* Incorrect solution */
+// class Solution {
+//     public int shortestDistance(String[] words, String word1, String word2) {
+//         int i = -1, j = -1;
+//         int min = Integer.MAX_VALUE;
+//         for(int k = 0; k < words.length; i++) {
+//             if(words[k].equals(word1)) i = k;
+//             else if(words[k].equals(word2)) j = k;
+//             if(i != -1 && j != -1)
+//                 min = Math.min(min, Math.abs(i - j));
+//         }
+//     }
+// }
 
-/* Efficient Solution than the previous one */
+/* Correct Solution - Check min condition only if word matches */
 class Solution {
     public int shortestDistance(String[] words, String word1, String word2) {
         int i = -1, j = -1;
@@ -51,7 +51,7 @@ class Solution {
                 if(i != -1 && j != -1)
                     min = Math.min(min, Math.abs(i - j));
             }
-            else if(words[k].equals(word1)) {
+            else if(words[k].equals(word2)) {
                 j = k;
                 if(i != -1 && j != -1)
                     min = Math.min(min, Math.abs(i - j));
@@ -62,6 +62,6 @@ class Solution {
 
 /***
 
-
+Note: You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
 
 **/
