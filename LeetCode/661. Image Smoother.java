10b0f25a0454158e1a5c986c542600197f201f94
Mehta <dmehta9@uncc.edu>
2018-11-05 13:00:41 -0500

    Matrix Image Manipulation

---
+++ LeetCode/661. Image Smoother.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int[][] imageSmoother(int[][] M) {
+        int m = M.length;
+        int n = M[0].length;
+        int[][] sumArr = new int[m][n];
+        
+        for(int i = 0; i < m; i++) {
+            int count = 1;
+            for(int j = 0; j < n; j++) {
+                if((i - 1) >= 0 && (j - 1) >= 0) {
+                    sumArr[i][j] += M[i - 1][j - 1];
+                    count += 1;
+                }
+                if((i - 1) >= 0) {
+                    sumArr[i][j] += M[i - 1][j];
+                    count += 1;
+                }
+                if((i - 1) >= 0 && (j + 1) < n) {
+                    sumArr[i][j] += M[i - 1][j + 1];
+                    count += 1;
+                }
+                if((j - 1) >= 0) {
+                    sumArr[i][j] += M[i][j - 1];
+                    count += 1;
+                }
+                if((j + 1) < n) {
+                    sumArr[i][j] += M[i][j + 1];
+                    count += 1;
+                }
+                if((i + 1) < m && (j - 1) >= 0) {
+                    sumArr[i][j] += M[i + 1][j - 1];
+                    count += 1;
+                }
+                if((i + 1) < m && (j + 1) < n) {
+                    sumArr[i][j] += M[i + 1][j + 1];
+                    count += 1;
+                }
+                if((i + 1) < m) {
+                    sumArr[i][j] += M[i + 1][j];
+                    count += 1;
+                }
+                sumArr[i][j] += M[i][j];
+                sumArr[i][j] = sumArr[i][j]/count;    
+            }
+        }
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(M[i][j] + " ");
+            }
+            System.out.println();
+        }
+         System.out.println();
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(sumArr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        return sumArr;
+    }
+}
