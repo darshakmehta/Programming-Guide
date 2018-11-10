68d23549d618d4a9199a9981ce592be89022ffdb
tech-boy <darshak_mehta@live.com>
2018-11-10 00:02:34 -0500

    Sum to the left and right of pivot is same

---
+++ LeetCode/724. Find Pivot Index.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        if(nums.length == 0) return -1;
+        
+        int n = nums.length;
+        int[] leftsum = new int[n], rightsum = new int[n];
+        
+        leftsum[0] = nums[0];
+        for(int i = 1; i < n; i++) {
+            leftsum[i] += nums[i] + leftsum[i - 1];
+        }
+        
+        rightsum[n - 1] = nums[n - 1];
+        for(int i = n - 2; i >= 0; i--) {
+            rightsum[i] += nums[i] + rightsum[i + 1];
+        }
+        
+        if(rightsum[1] == 0)
+            return 0;
+        
+        for(int i = 0; i < n - 2; i++) {
+            if(leftsum[i] == rightsum[i + 2]) {
+                return i + 1;
+            }
+        }
+        
+        if(leftsum[n - 2] == 0)
+            return n - 1;
+        
+        
+        return -1;
+    }
+}
