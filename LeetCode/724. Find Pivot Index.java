12797d7de629a4472db76131460628b67059cd7a
tech-boy <darshak_mehta@live.com>
2018-11-10 00:14:32 -0500

    Faster Solution O(1) space

--- LeetCode/724. Find Pivot Index.java
+++ LeetCode/724. Find Pivot Index.java
@@ -31,3 +31,29 @@ class Solution {
         return -1;
     }
 }
+
+/* Faster Solution 14ms 100% solution */
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        if(nums.length == 0) return -1;
+        
+        int n = nums.length;
+        int totalsum = 0, leftsum = 0;
+        
+        for(int i : nums)
+            totalsum += i;
+        
+        for(int i = 0; i < n; i++) {
+            if(leftsum == (totalsum - nums[i]))
+                return i;
+            else {
+                leftsum += nums[i];
+                totalsum -= nums[i];
+                
+            }
+        }
+        
+        return -1;
+    }
+}
