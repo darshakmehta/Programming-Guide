c8a0c15da39e29846ea0050bb7178ab29a346c0b
tech-boy <darshak_mehta@live.com>
2018-11-09 21:15:41 -0500

    Minimum cost at position i to jump 0 or 1 step

--- LeetCode/746. Min Cost Climbing Stairs.java
+++ LeetCode/746. Min Cost Climbing Stairs.java
@@ -16,12 +16,9 @@ class Solution {
 
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        int a = cost[0];
-        int b = cost[1];
-        int c = 0;
-        for(int i = 2; i < n; i++) {
-            c = Math.min(a, b) + cost[i];
+        int a = cost[0], b = cost[1];
+        for(int i = 2; i < cost.length; i++) {
+            int c = Math.min(a, b) + cost[i];
             a = b;
             b = c;
         }    
