a2c6190406f392667777adbd82b41f594885ab39
tech-boy <darshak_mehta@live.com>
2018-11-11 09:24:41 -0500

    Sum of nodes between L and R in BST

---
+++ LeetCode/938. Range Sum of BST.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if(root == null) return 0;
+        if(root.val >= L && root.val <= R)
+            return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+        else
+            return rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+    }
+}
