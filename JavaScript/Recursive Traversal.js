0d71930b2254b3f09f524b3f39f9ee76802cbe2e
tech-boy <darshak_mehta@live.com>
2018-11-22 21:21:17 -0500

    Recursive traversal

---
+++ JavaScript/Recursive Traversal.js
@@ -0,0 +1,34 @@
+var obj = [
+    { name: 'Darshak', id: 1},
+    [
+      { name: 'Indrajeet', id: 1},
+      [
+        { name: 'Mehta', id: 1},
+        { name: 'Love', id: 1},
+        [
+          { name: 'Mehta', id: 1},
+          { name: 'Love', id: 1},
+          [
+          [
+          [
+            { name: 'one', id: 1},
+            { name: 'Two', id: 1},
+          ]
+          ]
+          ]
+        ]
+      ]
+    ]
+  ];
+
+var printArr = (arr) => {
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i].length === undefined) {
+      console.log(arr[i].name + " " + arr[i].id);
+    } else {
+      printArr(arr[i]);
+    }
+  }
+}
+
+printArr(obj);
