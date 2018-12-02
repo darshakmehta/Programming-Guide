617c31d7ffa02f468c6e41bfa7b598b2676d75e6
tech-boy <darshak_mehta@live.com>
2018-12-02 08:39:44 -0500

    NOV18 Print YES

---
+++ CodeChef/Practice/November Long Challenge 2018/Chef and Difficult Contests.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Solution
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t != 0) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			/* In every case it is possible */
+			System.out.println("YES");	
+			t--;
+		}
+	}
+}
