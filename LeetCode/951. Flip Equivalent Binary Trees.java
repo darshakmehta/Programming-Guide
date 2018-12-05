9eea8184e9439ec410470ec43bd87456a24cff9e
tech-boy <darshak_mehta@live.com>
2018-12-04 23:29:15 -0500

    Canonical Representation of Trees

---
+++ LeetCode/951. Flip Equivalent Binary Trees.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null) return root1 == root2;
+        if (root1.val != root2.val) return false;
+
+        return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right) ||
+                flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+    }
+}
+
+/* Canonical Representation */
+
+/* Only applied when there are on duplicate values */
+
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        List<Integer> vals1 = new ArrayList();
+        List<Integer> vals2 = new ArrayList();
+        dfs(root1, vals1);
+        dfs(root2, vals2);
+        return vals1.equals(vals2);
+    }
+
+    public void dfs(TreeNode node, List<Integer> vals) {
+        if (node != null) {
+            vals.add(node.val);
+            int L = node.left != null ? node.left.val : -1;
+            int R = node.right != null ? node.right.val : -1;
+            /* Flip each node so that the left child is smaller than the right, and call this the canonical representation. All equivalent trees have exactly one canonical representation. */
+            if (L < R) {
+                dfs(node.left, vals);
+                dfs(node.right, vals);
+            } else {
+                dfs(node.right, vals);
+                dfs(node.left, vals);
+            }
+        }
+    }
+}
