371158e411bf8b36f6f5fb53dd005bb1dbaaa58e
tech-boy <darshak_mehta@live.com>
2018-12-25 19:54:30 -0500

    Reverse a string

--- LeetCode/344. Reverse String.java
+++ LeetCode/344. Reverse String.java
@@ -22,13 +22,13 @@ class Solution {
     }
 }
 
-class Solution { //Efficient than above 2 solutions
-    public String reverseString(String s) {
+class Solution { //Method 4: Efficient than above 2 solutions
+    public String reverseString(String s) { // O(n) ==> n/2 times
         char ch[] = s.toCharArray();
         int i = 0;
         int j = s.length() - 1;
         
-        while(i < j) {
+        while(i < j) { //Loop is prune to n/2
             char temp = ch[i];
             ch[i] = ch[j];
             ch[j] = temp;
@@ -39,11 +39,23 @@ class Solution { //Efficient than above 2 solutions
     }
 }
 
-/*** Method 1: Converting String into Bytes ***/
+class Solution { // Inefficient as compare to above efficient solution, as we loop entire string
+    public String reverseString(String s) {
+        
+        char[] result = new char[s.length()];
+        
+        for( int i = result.length - 1, k = 0; i >= 0 ; i--, k++) {
+            result[i] = s.charAt(k);
+        }
+        
+        return String.valueOf(result);
+     }
+}
+
+/*** TODO: Method 1: Converting String into Bytes ***/
 /*** Method 2: Using built in reverse() method of the StringBuilder class {Solution 1} ***/ 
-/*** Method 3: Converting String to character array {Solution 3} ***/ 
-/*** Method 4: Convert the input string into character array by using the toCharArray() and Reverse a String using swapping ***/
+/*** Method 3: Converting String to character array  ***/ 
+/*** Method 4: Convert the input string into character array by using the toCharArray() and Reverse a String using swapping {Solution 3} ***/
 /*** Method 5: Using ArrayList object or StringBuffer.append() {Solution 2} ***/
 
 //Reference: geeksforgeeks
-
