a4b0e9264ff2cf865a95a8b341e3ccf588fbb951
tech-boy <darshak_mehta@live.com>
2018-12-28 18:37:00 -0500

    TODO: Juggling

--- LeetCode/189. Rotate Array.java
+++ LeetCode/189. Rotate Array.java
@@ -26,3 +26,34 @@ class Solution { //efficient
         }
     }
 }
+
+/* Juggling Algorithm */
+
+class Solution {
+    public int gcd(int a, int b) { 
+        if (b == 0) 
+            return a; 
+        else
+            return gcd(b, a % b); 
+    } 
+    
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int x = gcd(k, n);
+
+        if(nums == null || nums.length == 1) return;
+        for ( int i = 0; i < x; i++ ) {
+            
+            int temp = nums[n - i - 1];
+            int j = n - i - 1;
+            while( true ) {
+                int d = j - k;
+                if( d < 0 ) d += n;
+                if ( d == n - i - 1) break;
+                nums[j] = nums[d];
+                j = d;
+            }
+            nums[j] = temp; 
+        }
+    }
+}
