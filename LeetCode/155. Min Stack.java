3aa4be5388d8f38e1a9be91a6937908fb8a34826
tech-boy <darshak_mehta@live.com>
2019-01-05 00:38:48 -0500

    Using Two Stack, Remember: Compare Object value

--- LeetCode/155. Min Stack.java
+++ LeetCode/155. Min Stack.java
@@ -1,3 +1,5 @@
+// Solution 1 -  Memory inefficient
+
 class MinStack {
     
     Node top;
@@ -59,6 +61,54 @@ TODO: Using 2 stack, as saving one data field on each node, will take large spac
 Refer CTCI/Chapter 3 - Stacks and Queues/3.2 Stack Min.java
 
 ***/
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+// Solution 2
+
+/* Using Two Stack */
+
+class MinStack {
+    
+    Stack<Integer> stack, minStack;
+    
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<Integer>();
+        minStack = new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if( minStack.isEmpty() ) {
+            minStack.push(x);
+        } else if ( stack.peek() <= minStack.peek()) {
+            minStack.push(stack.peek());
+        }
+    }
+    
+    public void pop() {
+        if( stack.peek().equals(minStack.peek() )) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+       return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
 
 /**
  * Your MinStack object will be instantiated and called as such:
