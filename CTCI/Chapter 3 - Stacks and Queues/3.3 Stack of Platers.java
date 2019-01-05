6c861059bb76f59cefbf521cf4b99e49b126d21f
tech-boy <darshak_mehta@live.com>
2019-01-04 20:16:08 -0500

    Stack

--- CTCI/Chapter 3 - Stacks and Queues/3.3 Stack of Platers.java
+++ CTCI/Chapter 3 - Stacks and Queues/3.3 Stack of Platers.java
@@ -13,11 +13,11 @@ ArrayList<Stack> stack = new ArrayList<Stack>();
 
 3. Now to get push always, getLastStack() from ArrayList and check if it is full, if it is full, make new stack and add to ArrayList, else push the new value to getLastStack()
 
-4. While popping check for the getLastStack(), and pop value, once u r removing last element of stack, free the stack by removing from ArrayList.
+4. While popping check for the getLastStack(), and pop value, once you are removing last element of stack, free the stack by removing from ArrayList.
 
 
 Follow up: Implement popAt(int index)
 
-Here, index of the Stack is provided. Lets consider pop removes the top element of the first stack so we have rollover all the elements one position before, that is element at botton of second stack will move to the top of first stack. This will have high complexity, so we can discuss trade-offs and decide to keep the stack half empty to avoid such situations.
+Here, index of the Stack is provided. Lets consider pop removes the top element of the first stack so we have rollover all the elements one position before, that is element at bottom of second stack will move to the top of first stack. This will have high complexity, so we can discuss trade-offs and decide to keep the stack half empty to avoid such situations.
 
 ***/
