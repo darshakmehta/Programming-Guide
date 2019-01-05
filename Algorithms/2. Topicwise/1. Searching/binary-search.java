93e9bf202c53c4b79fa39894bb846ae2ec5c9713
tech-boy <darshak_mehta@live.com>
2019-01-05 12:22:55 -0500

    Update Binary Search for overflow

--- Algorithms/2. Topicwise/1. Searching/binary-search.java
+++ Algorithms/2. Topicwise/1. Searching/binary-search.java
@@ -31,35 +31,32 @@ class Solution
 	}
 	
 	public int binarySearch(int a[], int value){  //iterative
-		int low = 0;
-		int high = a.length - 1;
-		int mid;
-		while(low <=high){
-			mid =  (low + high) / 2;
+		int low = 0, high = a.length - 1;
+
+		while(low <= high){
+			int mid = low + (high - low) / 2;
+
 			if(a[mid] == value)
 				return mid;
-			else if(a[mid] < value){
+			else if(a[mid] < value)
 				low = mid + 1;
-			}
-			else if(a[mid] > value){
+			else if(a[mid] > value)
 				high = mid - 1;
-			}
 		}
 		return -1;
 	}
 
 	public int recursiveBinarySearch(int a[], int low, int high, int value){  //recursive
-		int mid;
-		while(low <= high){
-			mid =  (low + high) / 2;
-			if(a[mid] == value)
+
+		if ( low <= high) {
+			int mid = low + (high - low) / 2;
+
+			if ( a[mid] == value )
 				return mid;
-			else if(a[mid] < value){
+			if (a[mid] < value){
 				return recursiveBinarySearch(a, mid + 1, high, value); 
-			}
-			else if(a[mid] > value){
-				return recursiveBinarySearch(a, low, mid - 1, value);
-			}
+			
+			return recursiveBinarySearch(a, low, mid - 1, value);
 		}
 		return -1;
 	}
