393578afe14648b6293453c82bd6a5b6f1fdf634
tech-boy <darshak_mehta@live.com>
2019-01-07 21:40:56 -0500

    Bubble Sort

--- Algorithms/2. Topicwise/2. Sorting/1. Bubble Sort/bubble-sort-optimized.java
+++ Algorithms/2. Topicwise/2. Sorting/1. Bubble Sort/bubble-sort-optimized.java
@@ -2,27 +2,25 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Solution
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
+class Solution {
+	public static void main (String[] args) throws java.lang.Exception {
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
 		int a[] = new int[size];
-		for(int i = 0; i < size; i++)
+		for (int i = 0; i < size; i++)
 			a[i] = sc.nextInt();
 		Solution solution = new Solution();
 		solution.bubbleSortOptimized(a);
-		for(int i = 0 ;i < a.length; i++)
-		System.out.print(a[i] + " ");
+		for (int i = 0 ; i < a.length; i++)
+			System.out.print(a[i] + " ");
 	}
 	
-	public void bubbleSort(int a[]){
+	public void bubbleSort(int a[]) {
 		boolean swapped;
-		for(int i = 0; i < a.length - 1; i++){
+		for (int i = 0; i < a.length - 1; i++) {
 			swapped = false;
-			for(int j =  0; j< a.length - i -1; j++){
-				if(a[j] > a[j + 1]){
+			for (int j =  0; j < a.length - i - 1; j++) {
+				if (a[j] > a[j + 1]) {
 					int temp = a[j];
 					a[j] = a[j+1];
 					a[j+1] = temp;
@@ -40,10 +38,10 @@ class Solution
 		boolean isSorted = false;
 		int lastUnsorted = a.length - 1;
 
-		while(!isSorted) {
+		while (!isSorted) {
 			isSorted = true;
-			for(int i = 0; i < lastUnsorted; i++) {
-				if(a[i] > a[i+1]) {
+			for (int i = 0; i < lastUnsorted; i++) {
+				if (a[i] > a[i+1]) {
 					int temp = a[i];
 					a[i] = a[i+1];
 					a[i+1] = temp;
