fb2d8fcfbb206014a34e979cc826f9a12f2cf89f
tech-boy <darshak_mehta@live.com>
2019-01-12 19:51:37 -0500

    Insertion Sort

--- Algorithms/2. Topicwise/2. Sorting/2. Insertion Sort/insertion-sort-recusrive.java
+++ Algorithms/2. Topicwise/2. Sorting/2. Insertion Sort/insertion-sort-recusrive.java
@@ -2,32 +2,30 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Ideone
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
+class Ideone {
+	public static void main (String[] args) throws java.lang.Exception {
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
 		int a[] = new int[size];
-		for(int i = 0; i < size; i++)
+		for (int i = 0; i < size; i++)
 			a[i] = sc.nextInt();
 		Ideone ideone = new Ideone();
 		ideone.insertionSortRecursive(a, a.length);
-		for(int i = 0 ;i < a.length; i++)
-		System.out.print(a[i] + " ");
+		for (int i = 0; i < a.length; i++)
+			System.out.print(a[i] + " ");
 	}
 	
-	public void insertionSortRecursive(int a[], int n){
-		if(n <= 1)
-			return;
-		insertionSortRecursive(a, n-1);
+	public void insertionSortRecursive(int a[], int n) {
+		if(n <= 1) return;
+
+		insertionSortRecursive(a, n - 1);
 		int last = a[n-1];
-		int j = n-2;
+		int j = n - 2;
 		
-		while(j>=0 && a[j] > last){
-			a[j+1] = a[j];
+		while (j >= 0 && a[j] > last) {
+			a[j + 1] = a[j];
 			j--;
 		}
-		a[j+1] = last;
+		a[j + 1] = last;
 	}
 }
