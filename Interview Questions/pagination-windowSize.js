91772a7db1b09c181701d88cf0dd07326f14216e
tech-boy <darshak_mehta@live.com>
2019-01-14 13:27:37 -0500

    Pagination - Minimum WindowSize

---
+++ Interview Questions/pagination-windowSize.js
@@ -0,0 +1,29 @@
+let windowSize = 3;
+
+//Left
+console.log("1 2 3");
+console.log("6 1 2");
+console.log("5 6 1");
+console.log("4 5 6");
+console.log("3 4 5");
+console.log("2 3 4");
+console.log("1 2 3");
+
+// What do I need to do?
+
+// append (x - 1) where x is the element
+// NOTE: if (x) == 1 => (x - 1) = 6
+
+//Right
+console.log("1 2 3");
+console.log("2 3 4");
+console.log("3 4 5");
+console.log("4 5 6");
+console.log("5 6 1");
+console.log("6 1 2");
+console.log("1 2 3");
+
+// What do I need to do?
+
+// append (x + 1) where x is the element
+// NOTE: if (x) == 6 => (x + 1) = 1
