b6db63dc9ba7701b63e69d1673ce1d1b2109a041
tech-boy <darshak_mehta@live.com>
2019-01-17 19:13:47 -0500

    BackTracking Add Operators to find Target

--- LeetCode/282. Expression Add Operators.java
+++ LeetCode/282. Expression Add Operators.java
@@ -1,34 +1,34 @@
 class Solution {
     List<String> result = new ArrayList<String>(); // to avoid passing in recursive stack
-    
-    public List<String> addOperators(String num, int N) {
+    public List<String> addOperators(String num, int target) {
         if ( num == null || num.length() == 0 ) 
             return result;
-        backtrack( new StringBuilder(), num, N, 0, 0, 0 );
+        backtrack( new StringBuilder(), num, target, 0, 0, 0 );
         return result;
     }
-    public void backtrack(StringBuilder sb, String num, int N, int index, long eval, long multiply) {
-        if ( index == num.length() ) {
-            if ( N == eval )
+    public void backtrack(StringBuilder sb, String num, int target, int pos, long eval, long multiply) {
+        if ( pos == num.length() ) {
+            if ( target == eval )
                 result.add( sb.toString() );
             return;
         }
-        for ( int i = index; i < num.length(); i++ ) {
-            if( i != index && num.charAt(index) == '0' ) break; // avoid leading zero
-            long current = Long.parseLong( num.substring( index, i + 1 ) ); //Long to avoid overflow
-            int len = sb.length(); // rem length to backtrack
-            if( index == 0 ) {
-                backtrack(sb.append(current), num, N, i + 1, current, current);
+        for ( int i = pos; i < num.length(); i++ ) {
+            if( i != pos && num.charAt(pos) == '0' ) break; // avoid leading zero
+            long current = Long.parseLong( num.substring( pos, i + 1 ) ); //Long to avoid overflow
+            int len = sb.length();
+            if( pos == 0 ) {
+                backtrack(sb.append(current), num, target, i + 1, current, current);
                 sb.setLength(len);
             }
             else {
-                backtrack( sb.append( "+" + current ), num, N, i + 1, eval + current , current );
+                backtrack( sb.append("+").append(current), num, target, i + 1, eval + current , current );
                 sb.setLength(len);
-                backtrack( sb.append( "-" + current ), num, N, i + 1, eval - current, -current );
+                backtrack( sb.append("-").append(current), num, target, i + 1, eval - current, -current );
                 sb.setLength(len);
-                backtrack( sb.append( "*" + current ), num, N, i + 1, 
+                backtrack( sb.append("*").append(current), num, target, i + 1, 
                             eval - multiply + multiply * current, multiply * current ); // (1 + 2 + 3) - 3 + (3 * 4)
                 sb.setLength(len);
+                
             }
         }
     }
