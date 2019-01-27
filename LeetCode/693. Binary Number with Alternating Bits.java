aa6b84cf6160588ca4ed482ccaaee002d45518bc
tech-boy <darshak_mehta@live.com>
2019-01-26 21:46:36 -0500

    No Extra Space

--- LeetCode/693. Binary Number with Alternating Bits.java
+++ LeetCode/693. Binary Number with Alternating Bits.java
@@ -16,3 +16,48 @@ class Solution {
         return true;
     }
 }
+
+/* Not using List */
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        while (n != 0) {
+            sb.append(n % 2);
+            n = n / 2;
+        }
+        
+        for ( int i = 1; i < sb.length(); i++ ) {
+            if(sb.charAt(i) == sb.charAt(i - 1)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
+/* No extra space */
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        
+        int n1 = 0, n2 = 0;
+        boolean flag = false;
+        while (n != 0) {
+            n1 = n % 2;
+            n = n / 2;
+            if(flag && n1 == n2) {
+                 return false;
+            }
+            if(n == 0) break;
+            n2 = n % 2;
+            n = n / 2;
+            if(n1 == n2) return false;
+            flag = true;
+        }
+        
+        return true;
+    }
+}
