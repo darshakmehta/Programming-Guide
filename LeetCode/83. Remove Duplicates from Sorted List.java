942bb106eeb3db7c9e53f10cb5ee3f228c599793
tech-boy <darshak_mehta@live.com>
2019-02-08 23:18:00 -0500

    Remove Duplicates of a sortest list

--- LeetCode/83. Remove Duplicates from Sorted List.java
+++ LeetCode/83. Remove Duplicates from Sorted List.java
@@ -33,3 +33,23 @@ class Solution {
         return head;
     }
 }
+
+
+/* Efficient Solution */
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode curr = head;
+        while (curr != null && curr.next != null) {
+            if(curr.val == curr.next.val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+
+        return head;
+    }
+}
