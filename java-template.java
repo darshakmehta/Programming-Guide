3f3200e53583b33ba0fff97dca243f8fc369b223
tech-boy <darshak_mehta@live.com>
2019-02-10 09:48:27 -0500

    LCM added

--- java-template.java
+++ java-template.java
@@ -71,12 +71,15 @@ class Solution
         nums[b] = temp;
     }
     	
-	public int gcd(int a, int b) 
-    {
+	public int gcd(int a, int b) {
         if (b == 0) 
             return a; 
         else
             return gcd(b, a % b); 
+    }
+
+    public static int lcm(int a, int b) {
+        return a * (b / gcd(a, b));
     }
 
     public int sieveOfEratosthenes(int n) {
