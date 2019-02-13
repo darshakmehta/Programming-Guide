9c372d82712fbfb80e82a9de84e7307af3b24c5b
tech-boy <darshak_mehta@live.com>
2019-02-12 23:00:22 -0500

    Add number to array of numbers

---
+++ LeetCode/989. Add to Array-Form of Integer.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        
+        List<Integer> result = new ArrayList<Integer>();
+        
+        int carry = 0;
+        int i = A.length - 1;
+        for ( ; K != 0 && i >= 0; i-- ) {
+            int x = carry + A[i] + K % 10;
+            carry = x / 10;
+            
+            result.add(0, x % 10);
+                       
+            K = K / 10;
+        }
+        
+        for ( ; i >= 0; i-- ) {
+            int x = carry + A[i];
+            carry = x / 10;
+            
+            result.add(0, x % 10);
+        }
+
+        while ( K != 0 ) {
+            int x = carry + K % 10;
+            carry = x / 10;
+            
+            result.add(0, x % 10);
+            K = K / 10;
+        }
+    
+        if ( carry == 1 ) {
+            result.add(0, carry);
+        }
+        
+        return result;
+        
+    }
+}
