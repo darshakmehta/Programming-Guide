5f6ce2088745c1b531a70f33b7df51353790d05f
tech-boy <darshak_mehta@live.com>
2019-02-15 20:06:12 -0800

    KMP Pattern Searching

--- Algorithms/2. Topicwise/5. Pattern Searching/kmp-substring-search.java
+++ Algorithms/2. Topicwise/5. Pattern Searching/kmp-substring-search.java
@@ -3,7 +3,7 @@ class Solution {
 		String text = "abxabcabcaby";
 		String pattern = "abcaby";
 		char[] patternArr = pattern.toCharArray();
-		char[] textArr = pattern.toCharArray();
+		char[] textArr = text.toCharArray();
 
 		int p = pattern.length();
 
@@ -11,12 +11,12 @@ class Solution {
 		patternDp[0] = 0;
 
 		for ( int i = 1, j = 0; i < p; ) {
-			if( patternArr[i] == patternArr[j] ) {
+			if ( patternArr[i] == patternArr[j] ) {
 				patternDp[i] = j + 1;
 				j++;
 				i++;
 			} else {
-				if(j != 0) {
+				if (j != 0) {
 					j = patternDp[j - 1];
 				} else {
 					patternDp[i] = 0;
@@ -27,8 +27,8 @@ class Solution {
 
 		int i = 0;
 		int j = 0;
-		while( i < textArr.length && j < patternArr.length ) {
-			if( textArr[i] == patternArr[j] ) {
+		while ( i < textArr.length && j < patternArr.length ) {
+			if ( textArr[i] == patternArr[j] ) {
 				i++; j++;
 			} else {
 				if ( j != 0 ) {
