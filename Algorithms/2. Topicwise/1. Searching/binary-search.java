2b4bcc1320c35d5bb8439972537ad1a69a053d14
tech-boy <darshak_mehta@live.com>
2019-02-15 19:57:33 -0800

    Searching

--- Algorithms/2. Topicwise/1. Searching/binary-search.java
+++ Algorithms/2. Topicwise/1. Searching/binary-search.java
@@ -16,16 +16,16 @@ class Solution
 		int v = sc.nextInt();
 		Solution solution = new Solution();
 		int result = solution.binarySearch(a, v);
-		if(result == -1) {
+		if (result == -1) {
 			System.out.println("Value: " + v + " not found");
-		}else {
+		} else {
 			System.out.println("Value: "+ v + " found at index " + result);
 		}
 
 		int recursiveResult = solution.recursiveBinarySearch(a, 0, size - 1, v);
-		if(recursiveResult == -1) {
+		if (recursiveResult == -1) {
 			System.out.println("Value: " + v + " not found");
-		}else {
+		} else {
 			System.out.println("Value: "+ v + " found at index " + recursiveResult);
 		}
 	}
@@ -33,14 +33,14 @@ class Solution
 	public int binarySearch(int a[], int value){  //iterative
 		int low = 0, high = a.length - 1;
 
-		while(low <= high){
+		while (low <= high){
 			int mid = low + (high - low) / 2;
 
-			if(a[mid] == value)
+			if (a[mid] == value)
 				return mid;
-			else if(a[mid] < value)
+			else if (a[mid] < value)
 				low = mid + 1;
-			else if(a[mid] > value)
+			else if (a[mid] > value)
 				high = mid - 1;
 		}
 		return -1;
