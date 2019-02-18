fc60fa376cb5bc92cb223f6659f04c09eda36d2a
tech-boy <darshak_mehta@live.com>
2019-02-18 14:21:03 -0500

    Remove Sort

--- LeetCode/78. Subsets.java
+++ LeetCode/78. Subsets.java
@@ -2,7 +2,7 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         if(nums.length == 0) return result;
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
         backtrack(result, new ArrayList<>(), nums, 0);
         return result;
     }
