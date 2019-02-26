83d413a6e24f904a8bff6cd79455297350d02e71
tech-boy <darshak_mehta@live.com>
2019-02-25 23:33:32 -0500

    Binary Search O ( log n )

--- LeetCode/35. Search Insert Position.java
+++ LeetCode/35. Search Insert Position.java
@@ -10,8 +10,27 @@ class Solution {
     }
 }
 
-/***
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while ( low <= high ) {
+            int mid = low + (high - low) / 2;
+            
+            if ( nums[mid] == target ) {
+                return mid;
+            }
 
-TODO: Binary Search
+            if ( nums[mid] > target ) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
 
-***/
+        }
+
+        return low;
+    }
+}
