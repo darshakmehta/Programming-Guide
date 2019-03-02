86e580e1a6bc7668fa8b642e45c1893242ab8e24
tech-boy <darshak_mehta@live.com>
2019-03-01 23:47:38 -0500

    Boolean[] space optimized

--- LeetCode/771. Jewels and Stones.java
+++ LeetCode/771. Jewels and Stones.java
@@ -9,13 +9,35 @@ class Solution {
         
         int count = 0;
         int[] a = new int[255];
-        for (int i = 0; i < j.length(); i++)
+
+        for ( int i = 0; i < j.length(); i++ ) {
             a[j.charAt(i)] += 1;
+        }
         
-        for (int i = 0; i < s.length(); i++)
-            if (a[s.charAt(i)] > 0) {
+        for ( int i = 0; i < s.length(); i++ ) {
+            if ( a[s.charAt(i)] > 0 ) {
                 count++;
             }
+        }
+        
+        return count;
+    }
+}
+
+class Solution {
+    public int numJewelsInStones(String j, String s) {
+        
+        boolean[] flag = new boolean[128];
+
+        for ( char c : j.toCharArray() ) {
+            flag[c] = true;
+        }
+        
+        int count = 0;
+
+        for ( char c : s.toCharArray() ) {
+            if ( flag[c] ) count++;
+        }
         
         return count;
     }
