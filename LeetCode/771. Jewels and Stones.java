5b82355b8b20bad4d42e4e5108ece51b98ead38b
tech-boy <darshak_mehta@live.com>
2019-03-02 00:00:53 -0500

    Information about boolean

--- LeetCode/771. Jewels and Stones.java
+++ LeetCode/771. Jewels and Stones.java
@@ -1,11 +1,5 @@
-/***
-Brute way: O(S*J)
-
-Linear Solution: O(S+J)
-***/
-
 class Solution {
-    public int numJewelsInStones(String j, String s) {
+    public int numJewelsInStones(String j, String s) { // Linear Solution: O(J + S)
         
         int count = 0;
         int[] a = new int[255];
@@ -24,10 +18,10 @@ class Solution {
     }
 }
 
-class Solution {
-    public int numJewelsInStones(String j, String s) {
+class Solution { // 100% solution
+    public int numJewelsInStones(String j, String s) { // Linear Solution: O(J + S)
         
-        boolean[] flag = new boolean[128];
+        boolean[] flag = new boolean[128]; // Depending on JVM implementation takes space, not particularly defined
 
         for ( char c : j.toCharArray() ) {
             flag[c] = true;
@@ -42,3 +36,19 @@ class Solution {
         return count;
     }
 }
+
+/**
+
+Remember: 
+
+boolean: The boolean data type has only two possible values: true and false. Use this data type for simple flags that track true/false conditions. This data type represents one bit of information, but its "size" isn't something that's precisely defined.
+
+Reference: 
+
+1. https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+
+Important Readings:
+
+1. https://stackoverflow.com/a/605451/4489066
+
+*/
