b6af692bfae69cb9c7a795221e69a5a4cd3dde11
tech-boy <darshak_mehta@live.com>
2019-03-04 23:27:18 -0500

    Dangling pointer

--- LeetCode/929. Unique Email Addresses.java
+++ LeetCode/929. Unique Email Addresses.java
@@ -1,16 +1,24 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
         
-        Set<String> set = new HashSet<String>(); //To store Unique Email Address
+        Set<String> set = new HashSet<String>(); // To store Unique Email Address
         
         for(String s : emails) {
-            String[] arr = s.split("@"); //Split by @ to find the local names and domain name
-            
+            String[] arr = s.split("@"); // Split by @ to find the local names and domain name
             String localName = s.split("\\+")[0]; // Split the local name by '+' to ignore the later part after '+'
-            localName = localName.replace(".", ""); //Replace '.' by "" in the local name
+            
+            localName = localName.replace(".", ""); // Replace '.' by "" in the local name
             set.add(localName + "@" + arr[1]); // Join the First part of local name after splitting by '+' and second part of splitting by '@' and dont forget to join them by @
         }
         
-        return set.size(); //Number of Unique Email Address
+        return set.size(); // Number of Unique Email Address
     }
 }
+
+/**
+
+Remember:
+
+Line 8: java.util.regex.PatternSyntaxException: Dangling meta character '+' near index 0
+
+*/
