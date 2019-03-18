b57fb9bf788604b853308145d4e66be138123444
tech-boy <darshak_mehta@live.com>
2019-03-17 20:25:03 -0700

    1's Complement

---
+++ LeetCode/1012. Complement of Base 10 Integer.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int bitwiseComplement(int N) {
+        
+        if ( N == 0) return 1;
+        
+        StringBuffer sb = new StringBuffer();
+        StringBuffer sb2 = new StringBuffer();
+        
+        while ( N != 0 ) {
+            sb.append(N%2);
+            N = N / 2;
+        }
+        sb = sb.reverse();
+        for ( int i = 0; i < sb.length(); i++ ) {
+            sb2.append(((sb.charAt(i) - '0') == 1 ? 0 : 1));
+        }
+
+        int result = 0;
+        
+        for (int i = sb.length() - 1, j = 0; i >= 0 ; i--, j++) {
+            result += ( sb2.charAt(i) - '0' ) * Math.pow(2, j);
+        }
+        
+        return result;
+    }
+}
