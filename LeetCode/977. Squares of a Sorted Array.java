40aa85879b4a1892c9c04bbefd30b0434030d6ae
tech-boy <darshak_mehta@live.com>
2019-03-21 22:27:46 -0700

    Two pointers

--- LeetCode/977. Squares of a Sorted Array.java
+++ LeetCode/977. Squares of a Sorted Array.java
@@ -24,3 +24,23 @@ class Solution {
         return A;
     }
 }
+
+/* Reference: https://leetcode.com/problems/squares-of-a-sorted-array/discuss/221922/Java-two-pointers-O(N) */
+
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        int n = A.length;
+        int[] result = new int[n];
+        int i = 0, j = n - 1;
+        for (int p = n - 1; p >= 0; p--) {
+            if (Math.abs(A[i]) > Math.abs(A[j])) {
+                result[p] = A[i] * A[i];
+                i++;
+            } else {
+                result[p] = A[j] * A[j];
+                j--;
+            }
+        }
+        return result;
+    }
+}
