79eb77de522ee909b54d57342916e7a588a28bb0
tech-boy <darshak_mehta@live.com>
2020-01-11 15:38:40 -0800

    1. Two Sum

--- LeetCode/1. Two Sum.java
+++ LeetCode/1. Two Sum.java
@@ -1,9 +1,10 @@
+/* Brute Force */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             int x = target - nums[i];
-            for(int j = 0; j < nums.length && j != i; j++) {
-                if(x == nums[j]) {
+            for (int j = 0; j < nums.length && j != i; j++) {
+                if (x == nums[j]) {
                     return new int[]{i, j};
                 }
             }
@@ -12,20 +13,21 @@ class Solution {
     }
 }
 
-class Solution { //Very efficient Solution
+class Solution { // Efficient Solution
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> hmap = new HashMap<Integer, Integer>();
         
-        for(int i = 0; i < nums.length; i++) {
-            if(!hmap.containsKey(nums[i])) {
-                hmap.put(nums[i], i);
-            }
+        for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if(hmap.containsKey(complement) && hmap.get(complement) != i) {
+            if (hmap.containsKey(complement)) {
                 return new int[]{hmap.get(complement), i};
             }
+            
+            if (!hmap.containsKey(nums[i])) // To avoid adding later index of already visited element
+                hmap.put(nums[i], i);
         }
-        return new int[]{-1, -1};
+        return new int[]{-1, -1}; // not found
     }
 }
 
+/* Another Solution can be Binary Search */
