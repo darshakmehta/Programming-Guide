0238c30b99e6f7fd1791ab655f2459aab72061f3
darshakmehta <darshak.mehta@rallyhealth.com>
2021-01-24 18:33:04 -0800

    Print actual solution

--- GeeksForGeeks/Data Structures/1. Array/Optimization Problems/3. Find the subarray with least average.java
+++ GeeksForGeeks/Data Structures/1. Array/Optimization Problems/3. Find the subarray with least average.java
@@ -1,7 +1,7 @@
 /**
  * Sliding Window Technique
  * Define k size subarray starting at index 0 and compute its average
- * Traverse i = 0 ... n - 1 and slide the subarray by adding and removing element at both the ends and recompute average
+ * Traverse i = (0 + k) ... n - 1 and slide the subarray by adding and removing element at both the ends and recompute average
  *      if (new_average is smaller) update average = new_average and start_index = (i - k + 1)
  * Print subarray starting at start_index
  */
@@ -10,10 +10,8 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Solution
-{
-    public static void main (String[] args) throws java.lang.Exception
-    {
+class Solution {
+    public static void main(String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); // no. of elements
         int k = sc.nextInt(); // size of subarray
@@ -39,7 +37,7 @@ class Solution
             float average = initial_sum / k;
             int current_sum = initial_sum;
 
-            for (int i = k, j = 0; i < n;i++, j++) {
+            for (int i = k, j = 0; i < n; i++, j++) {
                 current_sum += arr[i] - arr[j];
                 float current_average = current_sum / k;
 
@@ -60,3 +58,68 @@ class Solution
 }
 
 // Solution: https://ideone.com/Hwobxg
+
+/**
+ * Optimization: Since we are looking to find the subarray, we can avoid calculating average.
+ */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Solution {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // no. of elements
+        int k = sc.nextInt(); // size of subarray
+        int[] arr = new int[n];
+
+        if (k > n) {
+            System.out.println("Cannot form subarray");
+            return;
+        }
+
+        int initial_sum = 0;
+        for (int i = 0, K = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (K < k) {
+                initial_sum += arr[i];
+            }
+            K++;
+        }
+
+
+        int start = 0;
+        if (n != k) {
+            int current_sum = initial_sum;
+
+            for (int i = k, j = 0; i < n; i++, j++) {
+                current_sum += arr[i] - arr[j];
+
+                if (current_sum < initial_sum) {
+                    start = i - k + 1;
+                    current_sum = initial_sum;
+                }
+            }
+        }
+
+        System.out.print("Least Average Subarray is : [");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[start + i] + " ");
+        }
+        System.out.println("]");
+
+    }
+}
+
+// Solution: https://ideone.com/F8kG2D
+
+/*
+Testcases:
+
+7 2
+3 7 5 20 -10 0 12
+
+7 3
+3 7 90 20 10 50 40
+ */
--- GeeksForGeeks/Data Structures/1. Array/Optimization Problems/4. Find the minimum distance between two numbers.java
+++ GeeksForGeeks/Data Structures/1. Array/Optimization Problems/4. Find the minimum distance between two numbers.java
@@ -2,10 +2,8 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Solution
-{
-    public static void main (String[] args) throws java.lang.Exception
-    {
+class Solution {
+    public static void main(String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int x = sc.nextInt();
@@ -31,9 +29,85 @@ class Solution
             }
         }
 
-        System.out.println("Minimum distance: " + result);
+        System.out.println("Minimum distance betweeen x: " + x + " and y: " + y + " is " + result);
     }
 }
 
 // Solution: https://ideone.com/608Qq4
-// TODO: Print actual Solution
+
+/**
+ * Print actual Solution
+ */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Pair {
+    int x;
+    int y;
+    int minDistance;
+
+    public Pair(int x, int y, int minDistance) {
+        this.x = x;
+        this.y = y;
+        this.minDistance = minDistance;
+    }
+}
+
+class Solution {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int last_x = -1, last_y = -1;
+        Pair pair = new Pair(-1, -1, Integer.MAX_VALUE);
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x) {
+                last_x = i;
+            } else if (arr[i] == y) {
+                last_y = i;
+            }
+
+            if (last_x != -1 && last_y != -1) {
+                int distance = Math.abs(last_x - last_y);
+                if (pair.minDistance > distance) {
+                    pair.minDistance = distance;
+                    pair.x = last_x;
+                    pair.y = last_y;
+                }
+            }
+        }
+
+        System.out.println("Minimum distance betweeen x: " + x + " and y: " + y + " is " + pair.minDistance);
+
+        System.out.println("x: " + x + " is found at index: " + pair.x);
+        System.out.println("y: " + y + " is found at index: " + pair.y);
+    }
+}
+
+// Solution: https://ideone.com/fHauPl
+
+/*
+Testcases:
+
+8 3 2
+2 5 3 5 4 4 2 3
+
+2 1 2
+1 2
+
+3 3 5
+3 4 5
+
+12 3 6
+3 5 4 2 6 5 6 6 5 4 8 3
+ */
