34f1caeaa30736574939134495203a6879eb8608
darshakmehta <darshak.mehta@rallyhealth.com>
2022-06-28 23:25:16 -0700

    Duplicate number

--- LeetCode/287. Find the Duplicate Number.java
+++ LeetCode/287. Find the Duplicate Number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Arrays.sort(nums); //TODO: copy to temp array as nums is read only
+        Arrays.sort(nums); // TODO: copy to temp array as nums is read only
         for(int i = 0; i < nums.length - 1; i++) {
             if(nums[i] == nums[i + 1]) {
                 return nums[i];
@@ -10,25 +10,25 @@ class Solution {
     }
 }
 
-class Solution { //Faster Approach
+class Solution {
     public int findDuplicate(int[] nums) {
         int tortoise = nums[0];
         int hare = nums[0];
-        
+
         do {
             tortoise = nums[tortoise];
             hare = nums[nums[hare]];
         } while(tortoise != hare);
-        
-        int ptr1 = nums[0];
-        int ptr2 = tortoise;
-        
-        while(ptr1 != ptr2) {
-            ptr1 = nums[ptr1];
-            ptr2 = nums[ptr2];
+
+        int slow = nums[0];
+        int fast = hare;
+
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        
-        return ptr1;
+
+        return slow;
     }
 }
 
@@ -40,7 +40,7 @@ Here, each number in nums is a "pigeon" and each distinct number that can appear
 
 TODO:
 
-1. Sorting will rearraange array so it does not satisfy the constraints. [O(nlogn) and O(n) space] 
+1. Sorting will rearrange array so it does not satisfy the constraints. [O(nlogn) and O(n) space]
 2. HashSet approach much faster O(n) and O(n) space. If already exist in set that is the duplicate element.
 *** Think about PigeonHole principle ***
 3. Approach using Constant space and read only array --> Floyd's Tortoise and Hare (Cycle Detection)
