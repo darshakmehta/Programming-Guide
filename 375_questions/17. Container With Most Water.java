f4a20be1e8846fe06a184abc51eec4667b88fe8d
darshakmehta <darshak.mehta@rallyhealth.com>
2022-07-02 23:55:21 -0700

    Container With Most Water

--- 375_questions/17. Container With Most Water.java
+++ 375_questions/17. Container With Most Water.java
@@ -1,24 +1,5 @@
 class Solution {
     public int maxArea(int[] height) {
-        int water = 0;
-        int left = 0;
-        int right = height.length - 1;
-
-        while (left < right) {
-            water = Math.max(water, Math.min(height[left], height[right]) * (right - left));
-            if (height[left] < height[right]) {
-                left++;
-            }
-            else {
-                right--;
-            }
-        }
-        return water;
-    }
-}
-
-class Solution {
-    public int maxArea(int[] height) {
         int n = height.length;
         int area = 0;
         int left = 0, right = n - 1;
