18c8c849041e557d2bf8cb4cf5dd74fcdabd34cb
darshakmehta <darshak.mehta@rallyhealth.com>
2022-07-03 08:46:15 -0700

    Trapping Rain Water

---
+++ LeetCode/42. Trapping Rain Water.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int left = 0, right = n - 1;
+        int maxLeft = 0, maxRight = 0;
+        int trapWater = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    trapWater += maxLeft - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    trapWater += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+        return trapWater;
+    }
+}
+
+/**
+Explanation: https://leetcode.com/problems/trapping-rain-water/discuss/153992/Java-O(n)-time-and-O(1)-space-(with-explanations).
+**/
