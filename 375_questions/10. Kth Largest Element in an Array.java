1bb266cf5169a2362949d465a556a3d187061caa
darshakmehta <darshak.mehta@rallyhealth.com>
2022-07-03 20:06:51 -0700

    Kth Largest Element in an Array

--- 375_questions/10. Kth Largest Element in an Array.java
+++ 375_questions/10. Kth Largest Element in an Array.java
@@ -1,12 +1,5 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
-    }
-}
-
-class Solution {
-    public int findKthLargest(int[] nums, int k) {
         final Queue<Integer> pq = new PriorityQueue<>();
 
         for (int val : nums) {
@@ -19,12 +12,3 @@ class Solution {
         return pq.peek();
     }
 }
-
-/***
-
-TODO:
-
-1. Heap Sort
-2. Quick Sort | Quick Select
-
-***/
