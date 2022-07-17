e7d7bd50d2c2ebe70ca3e43721416d147a81b2ee
darshakmehta <darshak.mehta@rallyhealth.com>
2022-07-17 16:54:52 -0700

    Count Palindromic Subsequences

---
+++ 375_questions/40. Count Palindromic Subsequences.java
@@ -0,0 +1,88 @@
+class Solution {
+    int count = 0;
+
+    public void extendPalindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++;
+            left--;
+            right++;
+        }
+    }
+
+    public int countSubstrings(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        for (int i = 0; i < s.length(); i++) { // i is the mid point
+            extendPalindrome(s, i, i); // odd length
+            extendPalindrome(s, i, i + 1); // even length
+        }
+        return count;
+    }
+}
+
+/* Print All Palindromic substrings */
+class Solution {
+    int count = 0;
+    List<String> list = new ArrayList<String>(); // List containing palindromic substrings
+
+    public void extendPalindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) { // Expand Around center
+            list.add(s.substring(left, right + 1));
+            count++;
+            left--;
+            right++;
+        }
+    }
+
+    public int countSubstrings(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            extendPalindrome(s, i, i);
+            extendPalindrome(s, i, i + 1);
+        }
+
+        for (String str : list)
+            System.out.println(str); // Print list of palindromic substrings
+        return count;
+    }
+    /* To print unique substring, add them to HashSet */
+}
+
+
+class Solution {
+    int count = 0;
+    Map<String, Integer> hmap = new HashMap<String, Integer>(); // to find all the occurrences or distinct occurrences of substring
+
+    public void extendPalindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            int count = 1;
+            if (hmap.containsKey(s.substring(left, right + 1))) {
+                count = hmap.get(s.substring(left, right + 1)) + 1;
+            }
+            hmap.put(s.substring(left, right + 1), count);
+            count++;
+            left--;
+            right++;
+
+        }
+    }
+
+    public int countSubstrings(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            extendPalindrome(s, i, i);
+            extendPalindrome(s, i, i + 1);
+        }
+
+        Set set = hmap.entrySet();
+        Iterator iter = set.iterator();
+        while (iter.hasNext()) {
+            Map.Entry pair = (Map.Entry) iter.next();
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+
+        return count;
+    }
+}
