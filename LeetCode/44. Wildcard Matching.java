a001da628daf2fb12160493d261dede839e0f6ca
darshakmehta <darshak.mehta@rallyhealth.com>
2022-07-17 19:25:32 -0700

    Wildcard matching

--- LeetCode/44. Wildcard Matching.java
+++ LeetCode/44. Wildcard Matching.java
@@ -1,47 +1,46 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        
-        if(s.equals("") && p.equals("")) return true;
+        if (s.equals("") && p.equals("")) return true;
         else if(!s.equals("") && p.equals("")) return false;
-        
+
         char[] stringArr = s.toCharArray();
         char[] arr = p.toCharArray();
-        
+
         StringBuilder sb = new StringBuilder(String.valueOf(arr[0]));
-        
+
         /* Transform a**b**c => a*b*c in pattern */
-        for ( int i = 1, j = 1; i < p.length(); i++) {
+        for (int i = 1, j = 1; i < p.length(); i++) {
             if (arr[i] == arr[i - 1]) {
-                if( arr[i] != '*') {
+                if (arr[i] != '*') {
                     sb.append(arr[i]);
                 }
             } else {
                 sb.append(arr[i]);
             }
         }
-        
-        char[] patternArr = sb.toString().toCharArray(); //New Pattern Array
-        
+
+        char[] patternArr = sb.toString().toCharArray(); // New Pattern Array
+
         int m = stringArr.length;
         int n = patternArr.length;
-        
+
         boolean[][] dp = new boolean[m + 1][n + 1];
-        
+
         dp[0][0] = true;
-        
-        if(n > 0 && patternArr[0] == '*')
+
+        if (n > 0 && patternArr[0] == '*')
             dp[0][1] = true;
 
-        for ( int i = 1; i < m + 1; i++ ) {
-            for ( int j = 1; j < n + 1; j++ ) {
-                if( stringArr[i - 1] == patternArr[j - 1] || patternArr[j - 1] == '?') {
+        for (int i = 1; i < m + 1; i++ ) {
+            for (int j = 1; j < n + 1; j++ ) {
+                if (stringArr[i - 1] == patternArr[j - 1] || patternArr[j - 1] == '?') {
                     dp[i][j] = dp[i - 1][j - 1];
-                } else if( patternArr[j - 1] == '*' ) {
+                } else if (patternArr[j - 1] == '*' ) {
                     dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                } 
-            }   
+                }
+            }
         }
-        
+
         /* Print DP */
         // for ( int i = 0; i < m + 1; i++ ) {
         //     for ( int j = 0; j < n + 1; j++ ) {
@@ -49,56 +48,50 @@ class Solution {
         //     }
         //     System.out.println();
         // }
-        
-        
-        
+
         return dp[m][n];
-        
     }
 }
 
 /* O(n) space solution */
 
-
-
-
 class Solution {
     public boolean isMatch(String s, String p) {
-        
+
         if(s.equals("") && p.equals("")) return true;
         else if(!s.equals("") && p.equals("")) return false;
         else if(s.equals("") && p.equals("*")) return true;
         else if(s.equals("") && p.equals("?")) return false;
         else if(s.equals("")) return false;
-        
+
         char[] stringArr = s.toCharArray();
         char[] arr = p.toCharArray();
-        
+
         StringBuilder sb = new StringBuilder(String.valueOf(arr[0]));
-        
+
         /* Transform a**b**c => a*b*c in pattern */
         for ( int i = 1, j = 1; i < p.length(); i++) {
             if (arr[i] == arr[i - 1]) {
-                if( arr[i] != '*') {
+                if (arr[i] != '*') {
                     sb.append(arr[i]);
                 }
             } else {
                 sb.append(arr[i]);
             }
         }
-        
+
         char[] patternArr = sb.toString().toCharArray(); //New Pattern Array
-        
+
         int m = stringArr.length;
         int n = patternArr.length;
-        
+
         boolean[] dp = new boolean[m + 1];
         dp[0] = true;
-        
+
         for (int i = 0; i < n; i++) {
             if (patternArr[i] == '*') {
                 for (int j = 0; j < m; j++) {
-                    dp[j + 1] = dp[j] || dp[j + 1]; 
+                    dp[j + 1] = dp[j] || dp[j + 1];
                 }
             } else {
                 for (int j = m - 1; j >= 0; j--) {
