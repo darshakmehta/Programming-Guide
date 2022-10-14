d32a8d83d892f49bf10a5355190dc77c4c68aa24
Darshak Mehta <darshak.mehta@rallyhealth.com>
2022-10-13 18:49:49 -0700

    310. Climbing Stairs

---
+++ 375_questions/310. Climbing Stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+
+        // Base case
+        dp[1] = 1;
+        if (n >= 2) {
+            dp[2] = 2;
+        }
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
