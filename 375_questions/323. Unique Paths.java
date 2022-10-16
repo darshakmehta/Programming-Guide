6161cdbbd9b85e932ef75193607a368a4ea94592
Darshak Mehta <darshak.mehta@rallyhealth.com>
2022-10-16 11:37:49 -0700

    323. Unique Paths

---
+++ 375_questions/323. Unique Paths.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m == 0 || n == 0) return 0;
+        if (m == 1 || n == 1) return 1;
+
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) dp[i] = 1; // Arrays.fill(dp,1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+}
--- LeetCode/62. Unique Paths.java
+++ LeetCode/62. Unique Paths.java
@@ -1,7 +1,7 @@
 class Solution {
 	public int uniquePaths(int m, int n) {
 	    double value = 1;
-	    for (int i = 1; i <= n - 1; i++) {
+        for (int i = 1; i < n; i++) {
 	        value *= ((double) (m + i - 1) / (double) i);
 	    }
 	    return (int) Math.round(value);
@@ -10,18 +10,18 @@ class Solution {
 
 class Solution {
     public int uniquePaths(int m, int n) {
-        if(m == 1 || n == 1)
-            return 1;
-        m--;
-        n--;
-        if(m < n) {              // Swap, so that m is the bigger number
+        if (m == 1 || n == 1) return 1;
+
+        m--; n--;
+        if (m < n) {              // Swap, so that m is the bigger number
             m = m + n;
             n = m - n;
             m = m - n;
         }
+
         long res = 1;
         int j = 1;
-        for(int i = m + 1; i <= m + n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
+        for (int i = m + 1; i <= m + n; i++, j++) { // Instead of taking factorial, keep on multiply & divide
             res *= i;
             res /= j;
         }
@@ -36,7 +36,7 @@ class Solution {
         if (m == 1 || n == 1) return 1;
         
         int[] dp = new int[n];
-        for (int i = 0; i < n; i++) dp[i] = 1; //Arrays.fill(dp,1);
+        for (int i = 0; i < n; i++) dp[i] = 1; // Arrays.fill(dp,1);
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 dp[j] += dp[j - 1];
