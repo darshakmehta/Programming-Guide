5cfc18f2d196b8b96df0b710e1ed766cdfa0e02c
Darshak Mehta <darshak.mehta@rallyhealth.com>
2022-10-16 22:38:18 -0700

    328. Edit distance

--- GeeksForGeeks/Algorithms/3. Algorithm Paradigm/2. Dynamic Programming/Edit Distance.java
+++ GeeksForGeeks/Algorithms/3. Algorithm Paradigm/2. Dynamic Programming/Edit Distance.java
@@ -2,25 +2,32 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Solution
-{
+class Solution {
 	public static int numberOfEdits(String s1, String s2) {
 		int m = s1.length();
 		int n = s2.length();
 		int dp[][] = new int[m + 1][n + 1];
 
-		for(int i = 0; i <= m; i++) {
-			for(int j = 0; j <= n; j++) {
-				if(i == 0)
-					dp[i][j] = j; //Min. operations = j
-				else if(j == 0)
-					dp[i][j] = i; //Min. operations = i
-				else if(s1.charAt(i - 1) == s2.charAt(j - 1)) 
+		for (int i = 0; i <= m; i++) {
+			for (int j = 0; j <= n; j++) {
+				// If first string is empty, only option is
+				// to insert all characters of second string
+				if (i == 0) {
+					dp[i][j] = j; // Min. operations = j
+				}
+				// If second string is empty, only option is
+				// to remove all characters of second string
+				else if (j == 0) {
+					dp[i][j] = i; // Min. operations = i
+				}
+				else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
 					dp[i][j] = dp[i - 1][j - 1];
-				else
-					dp[i][j] = 1 + Math.min(dp[i - 1][j -1], //replace 
-						   Math.min(dp[i - 1][j], // remove
-						   dp[i][j - 1])); //insert
+				}
+				else {
+					dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], // replace
+							Math.min(dp[i - 1][j], // remove
+									dp[i][j - 1])); // insert
+				}
 			}
 		}
 
